55e6c9789f1b6e549e9d9e90a3db3c7f17dacb27
alexlokshin-czi <alokshin@chanzuckerberg.com>
2022-02-10 15:34:28 -0500

    fix: Network configuration didn't have a complete list of subnets and security groups

---
+++ pkg/backend/aws/ecs_test.go
@@ -0,0 +1,33 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/chanzuckerberg/happy/pkg/config"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNetworkConfig(t *testing.T) {
+	r := require.New(t)
+	backend := Backend{}
+	sgs := []string{"sg-1", "sg-2"}
+	subnets := []string{"subnet-1", "subnet-2"}
+
+	backend.integrationSecret = &config.IntegrationSecret{
+		ClusterArn:     "arn:cluster",
+		PrivateSubnets: subnets,
+		SecurityGroups: sgs,
+		Services:       map[string]*config.RegistryConfig{},
+	}
+	networkConfig := backend.getNetworkConfig()
+	r.NotNil(networkConfig)
+	r.Equal(len(subnets), len(networkConfig.AwsvpcConfiguration.Subnets))
+	r.Equal(len(sgs), len(networkConfig.AwsvpcConfiguration.SecurityGroups))
+
+	for index, subnet := range subnets {
+		r.Equal(subnet, *networkConfig.AwsvpcConfiguration.Subnets[index])
+	}
+	for index, sg := range sgs {
+		r.Equal(sg, *networkConfig.AwsvpcConfiguration.SecurityGroups[index])
+	}
+}
