89ce489e4d0fa8b01ff3855fda4f24720d77383c
Alex Lokshin <96085118+alexlokshin-czi@users.noreply.github.com>
2022-02-11 13:12:00 -0500

    fix: Addressed addtags messaging an made flags required (#73)

--- pkg/artifact_builder/artifact_builder.go
+++ pkg/artifact_builder/artifact_builder.go
@@ -93,7 +93,7 @@ func (s *ArtifactBuilder) RetagImages(serviceRegistries map[string]*config.Regis
 
 		repoUrl := strings.Split(registry.GetRepoUrl(), "/")[1]
 
-		fmt.Printf("retagging %s from %s to %s", serviceName, sourceTag, destTags)
+		log.Printf("retagging %s from '%s' to '%s'", serviceName, sourceTag, strings.Join(destTags, ","))
 
 		input := &ecr.BatchGetImageInput{
 			ImageIds: []*ecr.ImageIdentifier{
