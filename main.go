bd23143b8f7d5a75eba90b6b56029613696e5048
Alex Lokshin <96085118+alexlokshin-czi@users.noreply.github.com>
2022-02-17 15:54:19 -0500

    feat: Audible alert upon failure (#89)
    * feat: Audible alert upon failure
    
    * Added a success notification
    
    * Missing imports

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"time"
+
 	"github.com/chanzuckerberg/happy/cmd"
 	"github.com/chanzuckerberg/happy/pkg/log"
+	"github.com/gen2brain/beeep"
 	"github.com/sirupsen/logrus"
 )
 
@@ -10,7 +13,12 @@ func main() {
 	logrus.SetLevel(logrus.InfoLevel)
 	logrus.SetFormatter(&log.Formatter{})
 
+	startTime := time.Now()
 	if err := cmd.Execute(); err != nil {
+		_ = beeep.Alert("Happy Error!", err.Error(), "assets/warning.png")
 		logrus.Fatal(err)
+	}
+	if time.Since(startTime) > 30*time.Second {
+		_ = beeep.Notify("Happy", "Successfully completed", "assets/information.png")
 	}
 }
