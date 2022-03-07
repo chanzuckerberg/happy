6b17313c9cf48d31abbe6e5a00410e766d59d03d
alexlokshin-czi <alokshin@chanzuckerberg.com>
2022-03-07 16:24:44 -0500

    Updated the test per code review

--- pkg/workspace_repo/tfe_token_test.go
+++ pkg/workspace_repo/tfe_token_test.go
@@ -11,14 +11,13 @@ import (
 func TestTfeToken(t *testing.T) {
 	req := require.New(t)
 
-	os.Unsetenv("TFE_TOKEN")
 	dir, err := os.Getwd()
 	req.NoError(err)
-	os.Setenv("HOME", path.Join(dir, "testdata"))
+	t.Setenv("HOME", path.Join(dir, "testdata"))
 	token, err := GetTfeToken("https://www.tfe.com")
 	req.NoError(err)
 	req.Equal("aaa.bbb.ccc", token)
-	os.Setenv("HOME", path.Join(dir, "testdata_nope"))
+	t.Setenv("HOME", path.Join(dir, "testdata_nope"))
 	_, err = GetTfeToken("https://www.tfe.com")
 	req.Error(err)
 }
