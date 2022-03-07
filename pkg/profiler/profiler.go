0c4315dbb823a1f6839df162f2a78b66d099cd08
dtsai-czi <100375199+dtsai-czi@users.noreply.github.com>
2022-03-07 11:21:14 -0800

    feat: Profile the runtime of happy create (#147)
    * Implement runtime profiling for happy create
    
    * Change Println to log.Info
    
    * Add profiler COVERAGE
    
    * Use human readable durations in profiler output
    
    * Update coverage

---
+++ pkg/profiler/profiler.go
@@ -0,0 +1,36 @@
+package profiler
+
+import (
+	"time"
+
+	units "github.com/docker/go-units"
+	log "github.com/sirupsen/logrus"
+)
+
+type runtime struct {
+	duration   time.Duration
+	sectorName string
+}
+
+type Profiler struct {
+	runtimes []runtime
+}
+
+func NewProfiler() *Profiler {
+	return &Profiler{}
+}
+
+func (p *Profiler) AddRuntime(startTime time.Time, sectorName string) {
+	sectorDuration := time.Since(startTime)
+	p.runtimes = append(p.runtimes,
+		runtime{
+			duration:   sectorDuration,
+			sectorName: sectorName})
+}
+
+func (p *Profiler) PrintRuntimes() {
+	log.Info("Profiler results:")
+	for _, runtime := range p.runtimes {
+		log.Infof("Sector %s: %s elapsed", runtime.sectorName, units.HumanDuration(runtime.duration))
+	}
+}
