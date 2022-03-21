c1a5259d0d965e2a8e13cb5f014e1841ab578cd7
Alex Lokshin <96085118+alexlokshin-czi@users.noreply.github.com>
2022-03-21 13:19:26 -0700

    Update cmd/get.go
    Co-authored-by: Eduardo Lopez <elopez@chanzuckerberg.com>

--- cmd/get.go
+++ cmd/get.go
@@ -59,7 +59,6 @@ var getCmd = &cobra.Command{
 
 		logrus.Infof("Retrieving stack '%s' from environment '%s'", stackName, happyConfig.GetEnv())
 
-		// TODO look for existing package for printing table
 		headings := []string{"Name", "Owner", "Tags", "Status", "URLs"}
 		tablePrinter := util.NewTablePrinter(headings)
 
