cfce81c779dbec1d98d7b5d4bfa849aa8d8c86e2
Alex Lokshin <alokshin@chanzuckerberg.com>
2022-04-08 11:30:22 -0400

    Updated coverage

--- pkg/util/platform.go
+++ pkg/util/platform.go
@@ -2,18 +2,19 @@ package util
 
 import (
 	"fmt"
-	"runtime"
 
 	"github.com/containerd/containerd/platforms"
 	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
+func normalizePlatfrom(platform v1.Platform) v1.Platform {
+	platform.OS = "linux"
+	return platforms.Normalize(platform)
+}
+
 func getUserPlatform() v1.Platform {
 	platform := platforms.DefaultSpec()
-	if runtime.GOOS == "darwin" || runtime.GOOS == "windows" {
-		platform.OS = "linux"
-	}
-	return platforms.Normalize(platform)
+	return normalizePlatfrom(platform)
 }
 
 func GetSystemPlatform(architecture string) string {
@@ -21,8 +22,7 @@ func GetSystemPlatform(architecture string) string {
 	if err != nil {
 		return fmt.Sprintf("error: %s", err.Error())
 	}
-	platform.OS = "linux"
-	return platforms.Format(platforms.Normalize(platform))
+	return platforms.Format(normalizePlatfrom(platform))
 }
 
 func GetUserPlatform() string {
