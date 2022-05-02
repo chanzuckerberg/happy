28b785018000f2b5eaf4d3fcb312597fe9595974
Alex Lokshin <alokshin@chanzuckerberg.com>
2022-05-02 12:29:39 -0400

    Updated dependencies and removed unused functions per code review

--- pkg/util/helper.go
+++ pkg/util/helper.go
@@ -31,12 +31,3 @@ func TagValueToString(value interface{}) string {
 		return fmt.Sprintf("%v", t)
 	}
 }
-
-func Contains(slice []string, value string) bool {
-	for _, item := range slice {
-		if item == value {
-			return true
-		}
-	}
-	return false
-}
