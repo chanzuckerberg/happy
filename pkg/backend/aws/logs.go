24029a1a50d22e7f5ae4ab87954a45dd821c85b4
Jake Heath <jheath@chanzuckerberg.com>
2022-06-03 16:17:29 -0700

    fix: CCIE-220 rate-limit to GetLogEvents

--- pkg/backend/aws/logs.go
+++ pkg/backend/aws/logs.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/time/rate"
 )
 
 type GetLogsFunc func(*cloudwatchlogs.GetLogEventsOutput, error) error
@@ -20,8 +22,16 @@ func (b *Backend) GetLogs(
 		b.cwlGetLogEventsAPIClient,
 		input,
 	)
+	// NOTE[JH]: According to cloudwatch documentation, GetLogEvents only
+	// allows 25 requets / second. This limiter is configured for that, with an
+	// initial bucket size.
+	limiter := rate.NewLimiter(rate.Limit(25), 2)
 	for paginator.HasMorePages() {
-		err := f(paginator.NextPage(ctx))
+		err := limiter.Wait(ctx)
+		if err != nil {
+			return errors.Wrap(err, "error waiting for the rate limit to fill back up")
+		}
+		err = f(paginator.NextPage(ctx))
 		if isStop(err) {
 			return nil
 		}
