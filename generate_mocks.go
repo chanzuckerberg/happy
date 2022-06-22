2177aa3b9435a8cdf1b9c2719ffc88ef7c63106e
Alex Lokshin <alokshin@chanzuckerberg.com>
2022-06-22 12:13:24 -0400

    Added DynamoDB mock generation

--- generate_mocks.go
+++ generate_mocks.go
@@ -11,3 +11,4 @@ package main
 //go:generate mockgen -destination=./pkg/backend/aws/interfaces/mock_aws_ecs_task_running_waiter.go -package=interfaces github.com/chanzuckerberg/happy/pkg/backend/aws/interfaces ECSTaskRunningWaiterAPI
 //go:generate mockgen -destination=./pkg/backend/aws/interfaces/mock_aws_ecs_task_stopped_waiter.go -package=interfaces github.com/chanzuckerberg/happy/pkg/backend/aws/interfaces ECSTaskStoppedWaiterAPI
 //go:generate mockgen -destination=./pkg/backend/aws/interfaces/mock_aws_logs.go -package=interfaces github.com/chanzuckerberg/happy/pkg/backend/aws/interfaces GetLogEventsAPIClient
+//go:generate mockgen -destination=./pkg/backend/aws/interfaces/mock_aws_dynamodb.go -package=interfaces github.com/chanzuckerberg/happy/pkg/backend/aws/interfaces DynamoDB
