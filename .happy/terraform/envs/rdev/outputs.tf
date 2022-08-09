8a53295905aa63df30be818b127b288853d38426
Hayden Spitzley <105455169+hspitzley-czi@users.noreply.github.com>
2022-08-09 14:56:31 -0400

    feat: bootstrap happy (#1)

---
+++ .happy/terraform/envs/rdev/outputs.tf
@@ -0,0 +1,4 @@
+output "url" {
+  value       = module.stack.url
+  description = "The URL endpoint for the service"
+}
---
+++ .happy/terraform/envs/staging/variables.tf
@@ -0,0 +1,31 @@
+variable image_tag {
+  type        = string
+  description = "Please provide an image tag"
+}
+
+variable image_tags {
+  type        = string
+  description = "Override the default image tags (json-encoded map)"
+  default     = "{}"
+}
+
+variable priority {
+  type        = number
+  description = "Listener rule priority number within the given listener"
+}
+
+variable stack_name {
+  type        = string
+  description = "Happy Path stack name"
+}
+
+variable happy_config_secret {
+  type        = string
+  description = "Happy Path configuration secret name"
+}
+
+variable wait_for_steady_state {
+  type        = bool
+  description = "Should terraform block until ECS reaches a steady state?"
+  default     = true
+}
