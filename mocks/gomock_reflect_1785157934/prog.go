16c056907f5f1c61f9634e1c64004f05f466b8ad
Jake Heath <jheath@chanzuckerberg.com>
2022-08-19 16:03:05 -0700

    lint

--- mocks/gomock_reflect_1785157934/prog.go
+++ mocks/gomock_reflect_1785157934/prog.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -19,13 +18,12 @@ var output = flag.String("output", "", "The output file name, or empty to use st
 func main() {
 	flag.Parse()
 
-	its := []struct{
+	its := []struct {
 		sym string
 		typ reflect.Type
 	}{
-		
-		{ "Workspace", reflect.TypeOf((*pkg_.Workspace)(nil)).Elem()},
-		
+
+		{"Workspace", reflect.TypeOf((*pkg_.Workspace)(nil)).Elem()},
 	}
 	pkg := &model.Package{
 		// NOTE: This behaves contrary to documented behaviour if the
