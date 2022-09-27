3dc4bcfcdf33036f84d223684ded1da4fe27c30f
David Tsai <dtsai@chanzuckerberg.com>
2022-09-27 13:13:50 -0700

    Use update path for force create

--- cmd/create.go
+++ cmd/create.go
@@ -151,18 +151,22 @@ func runCreate(cmd *cobra.Command, args []string) error {
 		}
 	}
 
+	options := stackservice.NewStackManagementOptions(stackName).WithHappyConfig(happyConfig).WithStackService(stackService).WithBackend(backend)
 	// if we already have a stack and "force" then use existing
 	var stackMeta *stackservice.StackMeta
 	if force && existingStack != nil {
+		logrus.Infof("stack '%s' already exists, it will be updated", stackName)
 		stackMeta, err = existingStack.Meta(ctx)
 		if err != nil {
 			return err
 		}
+		options = options.WithStackMeta(stackMeta)
+		return updateStack(ctx, options)
 	} else {
 		stackMeta = stackService.NewStackMeta(stackName)
 	}
 
-	options := stackservice.NewStackManagementOptions(stackName).WithHappyConfig(happyConfig).WithStackService(stackService).WithStackMeta(stackMeta).WithBackend(backend)
+	options = options.WithStackMeta(stackMeta)
 
 	// now that we have images, create all TFE related resources
 	return createStack(ctx, cmd, options)
