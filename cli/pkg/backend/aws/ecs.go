f098926fec2dfbb8a5749781b385f281d44a6e97
Alex Lokshin <alokshin@chanzuckerberg.com>
2022-10-05 16:08:32 -0400

    Refactor

--- cli/pkg/backend/aws/ecs.go
+++ cli/pkg/backend/aws/ecs.go
@@ -2,13 +2,10 @@ package aws
 
 import (
 	"context"
-	"fmt"
 	"strings"
 	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/arn"
-	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	ecstypes "github.com/aws/aws-sdk-go-v2/service/ecs/types"
 	"github.com/chanzuckerberg/happy/pkg/util"
@@ -200,22 +197,6 @@ func (ab *Backend) waitForTasksToStart(ctx context.Context, taskARNs []string) (
 	return *tasks, nil
 }
 
-// GetLogGroupStreamsForTasks is just like GetLogGroupStreamsForService, except it gets all the log group and log
-// streams associated with a one-off task, such as a migration or deletion task.
-func (ab *Backend) GetLogGroupStreamsForTasks(ctx context.Context, taskARNs []string) (string, []string, error) {
-	tasks, err := ab.waitForTasksToStart(ctx, taskARNs)
-	if err != nil {
-		return "", nil, err
-	}
-
-	logConfigs, err := ab.getAWSLogConfigsFromTasks(ctx, tasks...)
-	if err != nil {
-		return "", nil, err
-	}
-
-	return logConfigs.GroupName, logConfigs.StreamNames, nil
-}
-
 func (ab *Backend) getTaskID(taskARN string) (string, error) {
 	resourceArn, err := arn.Parse(taskARN)
 	if err != nil {
@@ -239,92 +220,3 @@ const (
 	AwsLogsStreamPrefix = "awslogs-stream-prefix"
 	AwsLogsRegion       = "awslogs-region"
 )
-
-// getAWSLogConfigsFromTask grabs all the log groups and log streams for all containers for a particular task.
-func (ab *Backend) getAWSLogConfigsFromTask(ctx context.Context, task ecstypes.Task) (*AWSLogConfiguration, error) {
-	logConfigs := &AWSLogConfiguration{
-		StreamNames: []string{},
-	}
-	tdef, err := ab.ecsclient.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
-		TaskDefinition: aws.String(*task.TaskDefinitionArn),
-	})
-	if err != nil {
-		return nil, errors.Wrap(err, "could not describe task definition")
-	}
-
-	for _, container := range tdef.TaskDefinition.ContainerDefinitions {
-		// grab the awslogs logs-group and stream prefix from the task definition setting
-		// ignore containers that don't have this or are not set to use 'awslogs' driver
-		if container.LogConfiguration.LogDriver != "awslogs" {
-			continue
-		}
-		var (
-			awsLogStreamPrefix string
-			ok                 bool
-		)
-
-		logConfigs.GroupName, ok = container.LogConfiguration.Options[AwsLogsGroup]
-		if !ok {
-			continue
-		}
-
-		awsLogStreamPrefix, ok = container.LogConfiguration.Options[AwsLogsStreamPrefix]
-		// some tasks won't have a log stream prefix (a misconfiguration of their task definition)
-		// TODO: let's try to always have a prefix, otherwise, we run into this issue:
-		// https://docs.aws.amazon.com/AmazonECS/latest/developerguide/using_awslogs.html
-		// 	"If you don't specify a prefix with this option, then the log stream is named after the container ID
-		// 	that's assigned by the Docker daemon on the container instance. Because it's difficult to trace logs
-		// 	back to the container that sent them with just the Docker container ID (which is only available on
-		// 	the container instance), we recommend that you specify a prefix with this option."
-		if !ok {
-			streams, err := ab.cwlGetLogEventsAPIClient.DescribeLogStreams(ctx, &cloudwatchlogs.DescribeLogStreamsInput{
-				LogGroupName: &logConfigs.GroupName,
-				OrderBy:      "LastEventTime",
-				Descending:   aws.Bool(true),
-			})
-			if err != nil {
-				return nil, errors.Wrapf(err, "unable to get log streams from log group %s", logConfigs.GroupName)
-			}
-
-			for _, stream := range streams.LogStreams {
-				logConfigs.StreamNames = append(logConfigs.StreamNames, *stream.LogStreamName)
-			}
-		} else {
-			taskID, err := ab.getTaskID(*task.TaskArn)
-			if err != nil {
-				return nil, errors.Wrap(err, "unable to determine a task id")
-			}
-			// from the docs:
-			// "Use the awslogs-stream-prefix option to associate a log stream with the specified prefix, the container name, and the ID
-			// of the Amazon ECS task that the container belongs to. If you specify a prefix with this option, then the log stream takes the following format.
-			// prefix-name/container-name/ecs-task-id"
-			logConfigs.StreamNames = append(logConfigs.StreamNames, fmt.Sprintf("%s/%s/%s", awsLogStreamPrefix, *container.Name, taskID))
-		}
-	}
-
-	return logConfigs, nil
-}
-
-// getAWSLogConfigsFromTasks attempts to get all the log groups and log streams associated with the passed in
-// tasks. It makes an assumption that all the tasks passed in are a part of the same happy service and therefore
-// share a log group name. It only works for tasks that use the "awslog" driver.
-func (ab *Backend) getAWSLogConfigsFromTasks(ctx context.Context, tasks ...ecstypes.Task) (*AWSLogConfiguration, error) {
-	logConfigs := &AWSLogConfiguration{
-		GroupName:   "",
-		StreamNames: []string{},
-	}
-
-	for _, task := range tasks {
-		taskConfig, err := ab.getAWSLogConfigsFromTask(ctx, task)
-		if err != nil {
-			return nil, err
-		}
-		logConfigs.StreamNames = append(logConfigs.StreamNames, taskConfig.StreamNames...)
-		if logConfigs.GroupName == "" {
-			logConfigs.GroupName = taskConfig.GroupName
-		} else if logConfigs.GroupName != taskConfig.GroupName {
-			return nil, errors.Errorf("expected the log groups to be the same. got %s and %s", logConfigs.GroupName, taskConfig.GroupName)
-		}
-	}
-	return logConfigs, nil
-}
--- cli/pkg/backend/aws/ecs_compute_backend.go
+++ cli/pkg/backend/aws/ecs_compute_backend.go
@@ -3,9 +3,11 @@ package aws
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	ecstypes "github.com/aws/aws-sdk-go-v2/service/ecs/types"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
@@ -93,7 +95,7 @@ func (b *ECSComputeBackend) GetLogGroupStreamsForStack(ctx context.Context, stac
 		return "", nil, errors.Wrapf(err, "error getting task details for %+v", stackTaskARNs)
 	}
 
-	logConfigs, err := b.Backend.getAWSLogConfigsFromTasks(ctx, tasks...)
+	logConfigs, err := b.getAWSLogConfigsFromTasks(ctx, tasks...)
 	if err != nil {
 		return "", nil, err
 	}
@@ -141,11 +143,116 @@ func (b *ECSComputeBackend) RunTask(ctx context.Context, taskDefArn string, laun
 	}
 
 	log.Infof("task %s finished. printing logs from task", taskDefArn)
-	logGroup, logStreams, err := b.Backend.GetLogGroupStreamsForTasks(ctx, tasks)
+	logGroup, logStreams, err := b.getLogGroupStreamsForTasks(ctx, tasks)
 	if err != nil {
 		return err
 	}
 
 	p := util.MakeComputeLogPrinter(logGroup, logStreams, util.WithSince(util.GetStartTime(ctx).UnixMilli()))
 	return p.Print(ctx, b.Backend.cwlFilterLogEventsAPIClient)
+}
+
+// GetLogGroupStreamsForTasks is just like GetLogGroupStreamsForService, except it gets all the log group and log
+// streams associated with a one-off task, such as a migration or deletion task.
+func (b *ECSComputeBackend) getLogGroupStreamsForTasks(ctx context.Context, taskARNs []string) (string, []string, error) {
+	tasks, err := b.Backend.waitForTasksToStart(ctx, taskARNs)
+	if err != nil {
+		return "", nil, err
+	}
+
+	logConfigs, err := b.getAWSLogConfigsFromTasks(ctx, tasks...)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return logConfigs.GroupName, logConfigs.StreamNames, nil
+}
+
+// getAWSLogConfigsFromTasks attempts to get all the log groups and log streams associated with the passed in
+// tasks. It makes an assumption that all the tasks passed in are a part of the same happy service and therefore
+// share a log group name. It only works for tasks that use the "awslog" driver.
+func (b *ECSComputeBackend) getAWSLogConfigsFromTasks(ctx context.Context, tasks ...ecstypes.Task) (*AWSLogConfiguration, error) {
+	logConfigs := &AWSLogConfiguration{
+		GroupName:   "",
+		StreamNames: []string{},
+	}
+
+	for _, task := range tasks {
+		taskConfig, err := b.getAWSLogConfigsFromTask(ctx, task)
+		if err != nil {
+			return nil, err
+		}
+		logConfigs.StreamNames = append(logConfigs.StreamNames, taskConfig.StreamNames...)
+		if logConfigs.GroupName == "" {
+			logConfigs.GroupName = taskConfig.GroupName
+		} else if logConfigs.GroupName != taskConfig.GroupName {
+			return nil, errors.Errorf("expected the log groups to be the same. got %s and %s", logConfigs.GroupName, taskConfig.GroupName)
+		}
+	}
+	return logConfigs, nil
+}
+
+// getAWSLogConfigsFromTask grabs all the log groups and log streams for all containers for a particular task.
+func (b *ECSComputeBackend) getAWSLogConfigsFromTask(ctx context.Context, task ecstypes.Task) (*AWSLogConfiguration, error) {
+	logConfigs := &AWSLogConfiguration{
+		StreamNames: []string{},
+	}
+	tdef, err := b.Backend.ecsclient.DescribeTaskDefinition(ctx, &ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: aws.String(*task.TaskDefinitionArn),
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "could not describe task definition")
+	}
+
+	for _, container := range tdef.TaskDefinition.ContainerDefinitions {
+		// grab the awslogs logs-group and stream prefix from the task definition setting
+		// ignore containers that don't have this or are not set to use 'awslogs' driver
+		if container.LogConfiguration.LogDriver != "awslogs" {
+			continue
+		}
+		var (
+			awsLogStreamPrefix string
+			ok                 bool
+		)
+
+		logConfigs.GroupName, ok = container.LogConfiguration.Options[AwsLogsGroup]
+		if !ok {
+			continue
+		}
+
+		awsLogStreamPrefix, ok = container.LogConfiguration.Options[AwsLogsStreamPrefix]
+		// some tasks won't have a log stream prefix (a misconfiguration of their task definition)
+		// TODO: let's try to always have a prefix, otherwise, we run into this issue:
+		// https://docs.aws.amazon.com/AmazonECS/latest/developerguide/using_awslogs.html
+		// 	"If you don't specify a prefix with this option, then the log stream is named after the container ID
+		// 	that's assigned by the Docker daemon on the container instance. Because it's difficult to trace logs
+		// 	back to the container that sent them with just the Docker container ID (which is only available on
+		// 	the container instance), we recommend that you specify a prefix with this option."
+		if !ok {
+			streams, err := b.Backend.cwlGetLogEventsAPIClient.DescribeLogStreams(ctx, &cloudwatchlogs.DescribeLogStreamsInput{
+				LogGroupName: &logConfigs.GroupName,
+				OrderBy:      "LastEventTime",
+				Descending:   aws.Bool(true),
+			})
+			if err != nil {
+				return nil, errors.Wrapf(err, "unable to get log streams from log group %s", logConfigs.GroupName)
+			}
+
+			for _, stream := range streams.LogStreams {
+				logConfigs.StreamNames = append(logConfigs.StreamNames, *stream.LogStreamName)
+			}
+		} else {
+			taskID, err := b.Backend.getTaskID(*task.TaskArn)
+			if err != nil {
+				return nil, errors.Wrap(err, "unable to determine a task id")
+			}
+			// from the docs:
+			// "Use the awslogs-stream-prefix option to associate a log stream with the specified prefix, the container name, and the ID
+			// of the Amazon ECS task that the container belongs to. If you specify a prefix with this option, then the log stream takes the following format.
+			// prefix-name/container-name/ecs-task-id"
+			logConfigs.StreamNames = append(logConfigs.StreamNames, fmt.Sprintf("%s/%s/%s", awsLogStreamPrefix, *container.Name, taskID))
+		}
+	}
+
+	return logConfigs, nil
 }
