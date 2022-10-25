af18067321124b8b3da3a70f94347deec06bb843
Hayden Spitzley <hspitzley@chanzuckerberg.com>
2022-10-25 15:11:36 -0400

    configure golangci-lint

--- terraform/provider/main.go
+++ terraform/provider/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
+	"github.com/chanzuckerberg/happy/terraform/provider/pkg/provider"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-
-	"github.com/chanzuckerberg/happy/terraform/provider/pkg/provider"
 )
 
 func main() {
