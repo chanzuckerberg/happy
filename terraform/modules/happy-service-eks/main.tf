f4269eb20a4a791472cec8e9bf6daf579bf24920
Jake Heath <jheath@chanzuckerberg.com>
2022-11-29 12:11:12 -0800

    conflict

--- terraform/modules/happy-service-eks/main.tf
+++ terraform/modules/happy-service-eks/main.tf
@@ -61,14 +61,6 @@ resource "kubernetes_deployment" "deployment" {
         container {
           image = var.image
           name  = var.container_name
-<<<<<<< HEAD
-=======
-
-          env {
-            name  = "REMOTE_DEV_PREFIX"
-            value = var.remote_dev_prefix
-          }
->>>>>>> origin/main
           env {
             name  = "DEPLOYMENT_STAGE"
             value = var.deployment_stage
