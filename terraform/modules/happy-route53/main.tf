2012c1bc2b18c4e0e385a6557ecf2488d192d819
Hayden Spitzley <hspitzley@chanzuckerberg.com>
2022-12-06 14:24:46 -0800

    fix: specify provider in happy-route53 module

--- terraform/modules/happy-route53/main.tf
+++ terraform/modules/happy-route53/main.tf
@@ -11,15 +11,14 @@ resource "aws_route53_zone" "happy_route53_zone" {
 data "aws_route53_zone" "base" {
   name         = local.base_zone_name
   private_zone = false
-  providers = {
-    aws = aws.czi-si
-  }
+  provider     = aws.czi-si
 }
 
 resource "aws_route53_record" "happy" {
-  zone_id = data.aws_route53_zone.base.zone_id
-  name    = local.hosted_zone_name
-  type    = "NS"
-  ttl     = 300
-  records = aws_route53_zone.happy_route53_zone.name_servers
+  zone_id  = data.aws_route53_zone.base.zone_id
+  name     = local.hosted_zone_name
+  type     = "NS"
+  ttl      = 300
+  records  = aws_route53_zone.happy_route53_zone.name_servers
+  provider = aws.czi-si
 }
