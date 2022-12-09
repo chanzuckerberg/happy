2ed8a7d4a562872c69551d6a8490dbdb8b9625dd
Hayden Spitzley <hspitzley@chanzuckerberg.com>
2022-12-09 13:31:17 -0800

    add TODOs

--- terraform/modules/happy-stack-ecs/tags.tf
+++ terraform/modules/happy-stack-ecs/tags.tf
@@ -4,8 +4,8 @@ locals {
   tags = {
     happy_env           = var.deployment_stage,
     happy_stack_name    = var.stack_name,
-    happy_service_name  = var.service_name, // missing
-    happy_service_type  = var.service_type, // missing
+    happy_service_name  = "", // TODO: fill this is later
+    happy_service_type  = "", // TODO: fill this is later
     happy_region        = data.aws_region.current.name,
     happy_image         = local.image
     happy_last_applied  = timestamp(),
