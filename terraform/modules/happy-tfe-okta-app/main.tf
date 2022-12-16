705844ba31c061e0ecc015b39a6124bef6a86d1e
Hayden Spitzley <105455169+hspitzley-czi@users.noreply.github.com>
2022-12-16 16:25:11 -0500

    feat: make happy-tfe-okta-app more configurable (#888)

--- terraform/modules/happy-tfe-okta-app/main.tf
+++ terraform/modules/happy-tfe-okta-app/main.tf
@@ -1,13 +1,19 @@
 module "happy_apps" {
   for_each = var.envs
-  source   = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/okta-app-oauth?ref=v0.202.0"
+  source   = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/okta-app-oauth?ref=v0.232.0"
 
   okta = {
     label         = "${var.service_name}-${var.app_name}-${each.value}"
-    redirect_uris = ["https://oauth.${var.app_name}.${each.value}.si.czi.technology/oauth2/callback"]
-    login_uri     = "https://oauth.${var.app_name}.${each.value}.si.czi.technology"
+    redirect_uris = concat(["https://oauth.${var.app_name}.${each.value}.si.czi.technology/oauth2/callback"], var.redirect_uris)
+    login_uri     = var.login_uri == "" ? "https://oauth.${var.app_name}.${each.value}.si.czi.technology" : var.login_uri
     tenant        = "czi-prod"
   }
+
+  grant_types                = var.grant_types
+  app_type                   = var.app_type
+  token_endpoint_auth_method = var.token_endpoint_auth_method
+  omit_secret                = var.omit_secret
+
   tags = {
     owner   = "infra-eng@chanzuckerberg.com"
     service = "${var.service_name}-oauth"
