967d02f8179ad42b69319ba3ee2e0cd7545615ea
Jake Heath <jheath@chanzuckerberg.com>
2022-12-16 16:00:55 -0800

    claims and oidc verifiers working together

--- api/pkg/request/auth.go
+++ api/pkg/request/auth.go
@@ -2,12 +2,14 @@ package request
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/gofiber/fiber/v2"
 	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 // From https://github.com/dgrijalva/jwt-go/blob/master/request/oauth2.go
@@ -24,11 +26,80 @@ type OIDCVerifier interface {
 	Verify(ctx context.Context, idToken string) (*oidc.IDToken, error)
 }
 
-type MultiOIDCVerifier struct {
+type ClaimsVerifier interface {
+	MatchClaims(ctx context.Context, idToken *oidc.IDToken) error
+}
+
+type OIDCProvider struct {
+	oidcVerifier   OIDCVerifier
+	claimsVerifier ClaimsVerifier
+}
+
+type GithubClaimsVerifier struct {
+	owner  string
+	issuer string
+}
+
+type GithubClaims struct {
+	OIDCSubject string `json:"oidc_sub"`
+	OIDCExtra   string `json:"oidc_extra"`
+	Issuer      string `json:"iss"`
+}
+
+type NilClaimsVerifier struct {
+}
+
+func (d *NilClaimsVerifier) MatchClaims(ctx context.Context, idToken *oidc.IDToken) error {
+	return nil
+}
+
+var DefaultClaimsVerifier = &NilClaimsVerifier{}
+
+func MakeGithubClaimsVerifier(owner string) *GithubClaimsVerifier {
+	return &GithubClaimsVerifier{
+		owner:  owner,
+		issuer: "vstoken.actions.githubusercontent.com",
+	}
+}
+
+func (g *GithubClaimsVerifier) MatchClaims(ctx context.Context, idToken *oidc.IDToken) error {
+	claims := GithubClaims{}
+	err := idToken.Claims(&claims)
+	if err != nil {
+		return errors.Wrap(err, "github id token didn't have expected claims")
+	}
+
+	subject := fmt.Sprintf("repo:%s", g.owner)
+	if !strings.HasPrefix(claims.OIDCSubject, subject) {
+		return errors.Errorf("github id token didn't have the expected oidc_sub, expected to start with %s got %s", subject, claims.OIDCSubject)
+	}
+
+	if claims.Issuer != g.issuer {
+		return errors.Errorf("github id token didn't have the expected issuer, expected %s got %s", claims.Issuer, g.issuer)
+	}
+
+	return nil
+}
+
+func (o *OIDCProvider) Verify(ctx context.Context, rawIDToken string) (*oidc.IDToken, error) {
+	idToken, err := o.oidcVerifier.Verify(ctx, rawIDToken)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't verify the ID token")
+	}
+
+	err = o.claimsVerifier.MatchClaims(ctx, idToken)
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't verify the claims of the ID token")
+	}
+
+	return idToken, nil
+}
+
+type MultiOIDCProvider struct {
 	verifiers []OIDCVerifier
 }
 
-func (m *MultiOIDCVerifier) Verify(ctx context.Context, idToken string) (*oidc.IDToken, error) {
+func (m *MultiOIDCProvider) Verify(ctx context.Context, idToken string) (*oidc.IDToken, error) {
 	var errs error
 	for _, verifier := range m.verifiers {
 		idToken, err := verifier.Verify(ctx, idToken)
@@ -42,18 +113,23 @@ func (m *MultiOIDCVerifier) Verify(ctx context.Context, idToken string) (*oidc.I
 }
 
 func MakeMultiOIDCVerifier(verifiers ...OIDCVerifier) OIDCVerifier {
-	return &MultiOIDCVerifier{
+	return &MultiOIDCProvider{
 		verifiers: verifiers,
 	}
 }
 
-func MakeOIDCVerifier(ctx context.Context, issuerURL, clientID string) (OIDCVerifier, error) {
+func MakeOIDCProvider(ctx context.Context, issuerURL, clientID string, claimsVerifier ClaimsVerifier) (*OIDCProvider, error) {
 	provider, err := oidc.NewProvider(ctx, issuerURL)
 	if err != nil {
 		return nil, errors.Wrapf(err, "unable to create OIDC provider from %s", issuerURL)
 	}
 
-	return provider.Verifier(&oidc.Config{ClientID: clientID}), nil
+	return &OIDCProvider{
+		// there are cases where the issuer and the url value that issues the token are different (i.e. Github Actions)
+		// we skip these checks and use the claims verifier to verify the claims match what we expect
+		oidcVerifier:   provider.Verifier(&oidc.Config{ClientID: clientID, SkipIssuerCheck: true, SkipClientIDCheck: true}),
+		claimsVerifier: claimsVerifier,
+	}, nil
 }
 
 func validateAuthHeader(ctx context.Context, authHeader string, verifier OIDCVerifier) error {
@@ -77,6 +153,7 @@ func MakeAuth(verifier OIDCVerifier) fiber.Handler {
 
 		err := validateAuthHeader(c.Context(), authHeader, verifier)
 		if err != nil {
+			logrus.Info(err)
 			return c.SendStatus(fiber.StatusForbidden)
 		}
 		return c.Next()
