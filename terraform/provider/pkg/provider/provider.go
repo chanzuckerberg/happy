0c2f59a327b6d4ff5dc15d15dddb22f47667579c
Jake Heath <jheath@chanzuckerberg.com>
2022-12-19 14:35:49 -0800

    hook up provider code

--- terraform/provider/pkg/provider/provider.go
+++ terraform/provider/pkg/provider/provider.go
@@ -20,11 +20,11 @@ import (
 )
 
 type TFTokenProvider struct {
-	signingKey                           *rsa.PrivateKey
-	issuer, authorizationServerID, scope string
+	signingKey              *rsa.PrivateKey
+	issuer, audience, scope string
 }
 
-func MakeTFTokenProvider(rsaPrivateKey io.Reader, issuer, authorizationServerID, scope string) (*TFTokenProvider, error) {
+func MakeTFTokenProvider(rsaPrivateKey io.Reader, issuer, authzID, scope string) (*TFTokenProvider, error) {
 	b, err := io.ReadAll(rsaPrivateKey)
 	if err != nil {
 		return nil, errors.Wrap(err, "error reading private key")
@@ -34,10 +34,10 @@ func MakeTFTokenProvider(rsaPrivateKey io.Reader, issuer, authorizationServerID,
 		return nil, errors.Wrap(err, "unable to parse RSA private key")
 	}
 	return &TFTokenProvider{
-		signingKey:            signingKey,
-		issuer:                issuer,
-		authorizationServerID: authorizationServerID,
-		scope:                 scope,
+		signingKey: signingKey,
+		issuer:     issuer,
+		audience:   fmt.Sprintf("https://czi-prod.okta.com/oauth2/%s/v1/token", authzID),
+		scope:      scope,
 	}, nil
 }
 
@@ -52,13 +52,13 @@ func (t TFTokenProvider) GetToken() (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.RegisteredClaims{
 		Issuer:    t.issuer,
 		Subject:   t.issuer,
-		Audience:  []string{fmt.Sprintf("https://czi-prod.okta.com/oauth2/%s/v1/token", t.authorizationServerID)},
+		Audience:  []string{t.audience},
 		IssuedAt:  jwt.NewNumericDate(time.Now()),
 		ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour)),
 	})
 	signedToken, err := token.SignedString(t.signingKey)
 	if err != nil {
-		return "", errors.Wrapf(err, "error signing the JWT for %s %s", t.issuer, t.authorizationServerID)
+		return "", errors.Wrapf(err, "error signing the JWT for %s %s", t.issuer, t.audience)
 	}
 
 	values := url.Values{}
@@ -67,16 +67,15 @@ func (t TFTokenProvider) GetToken() (string, error) {
 	values.Add("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer")
 	values.Add("client_assertion", signedToken)
 
-	url := fmt.Sprintf("https://czi-prod.okta.com/oauth2/%s/v1/token", t.authorizationServerID)
 	params := values.Encode()
-	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(params))
+	req, err := http.NewRequest(http.MethodPost, t.audience, strings.NewReader(params))
 	if err != nil {
-		return "", errors.Wrapf(err, "error talking %s", url)
+		return "", errors.Wrapf(err, "error talking %s", t.audience)
 	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", errors.Wrapf(err, "error talking %s", url)
+		return "", errors.Wrapf(err, "error talking to %s", t.audience)
 	}
 
 	accessTokenResp := AccessTokenResponse{}
@@ -98,9 +97,33 @@ func Provider() *schema.Provider {
 			"api_base_url": {
 				Type:        schema.TypeString,
 				Required:    true,
+				Description: "The base URL for where the Happy API is located.",
 				DefaultFunc: schema.EnvDefaultFunc("HAPPY_API_BASE_URL", nil),
 			},
-			// TODO: add inputs needed for oidc
+			"api_private_key": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The authentication credentials in the form of a PEM encoded private key to authenticate to the Happy API.",
+				DefaultFunc: schema.EnvDefaultFunc("HAPPY_API_TOKEN", nil),
+			},
+			"api_oidc_issuer": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The OIDC issuer value that corresponds to the client ID of the Okta application associated with the private key.",
+				DefaultFunc: schema.EnvDefaultFunc("HAPPY_API_OIDC_ISSUER", nil),
+			},
+			"api_oidc_authz_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The Okta authorization server ID that authenticates the requests to the Happy API.",
+				DefaultFunc: schema.EnvDefaultFunc("HAPPY_API_OIDC_AUTHZ_ID", nil),
+			},
+			"api_oidc_scope": {
+				Type:        schema.TypeString,
+				Required:    false,
+				Description: "The required scope for the service account to authenticate properly.",
+				DefaultFunc: schema.EnvDefaultFunc("HAPPY_API_OIDC_SCOPE", "scope"),
+			},
 		},
 		ResourcesMap: map[string]*schema.Resource{},
 		DataSourcesMap: map[string]*schema.Resource{
@@ -111,7 +134,16 @@ func Provider() *schema.Provider {
 }
 
 func configureProvider(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-	apiBaseUrl := d.Get("api_base_url").(string)
-	api := client.NewHappyClient("happy-provider", version.ProviderVersion, apiBaseUrl, TFTokenProvider{})
+	apiBaseURL := d.Get("api_base_url").(string)
+	apiPrivateKey := d.Get("api_private_key").(string)
+	oidcIssuer := d.Get("api_oidc_issuer").(string)
+	authzID := d.Get("api_oidc_authz_id").(string)
+	scope := d.Get("api_oidc_scope").(string)
+
+	provider, err := MakeTFTokenProvider(strings.NewReader(apiPrivateKey), oidcIssuer, authzID, scope)
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+	api := client.NewHappyClient("happy-provider", version.ProviderVersion, apiBaseURL, provider)
 	return &APIClient{api: api}, nil
 }
