e3d7cf6ac4d9516372ba4c296f4b9086249e716f
Jake Heath <jheath@chanzuckerberg.com>
2022-12-20 15:53:56 -0800

    docs and output

--- terraform/modules/happy-tfe-okta-service-account/main.tf
+++ terraform/modules/happy-tfe-okta-service-account/main.tf
@@ -1,7 +1,14 @@
+# generate a signing key in terraform so that this
+# can be fully automatic and we don't have to worry about
+# passing keys around.
 resource "aws_kms_key" "service_user" {
   description              = local.label
   key_usage                = "SIGN_VERIFY"
   customer_master_key_spec = "RSA_4096"
+}
+resource "aws_kms_alias" "service_user" {
+  name_prefix         = "alias/${local.label}"
+  target_key_id = aws_kms_key.service_user.key_id
 }
 data "aws_kms_public_key" "service_user" {
   key_id = aws_kms_key.service_user.key_id
@@ -15,7 +22,10 @@ locals {
   label = "${var.service_name}-${var.app_name}-${var.env}-service-account"
 }
 
-module "happy_app" {
+# exact configuration needed to make a service user account
+# the output of this module will contain the KMS key's ID which can be used
+# to sign JWT for this service user.
+module "service_user" {
   source = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/okta-app-oauth?ref=heathj/jwks"
 
   okta = {
@@ -29,6 +39,7 @@ module "happy_app" {
   app_type                   = "service"
   token_endpoint_auth_method = "private_key_jwt"
   response_types             = ["token"]
+  grant_type_whitelist = ["client_credentials"]
 
   tags = {
     owner   = "infra-eng@chanzuckerberg.com"
@@ -37,7 +48,7 @@ module "happy_app" {
     env     = var.env
   }
   aws_ssm_paths = var.aws_ssm_paths
-  jwks          = local.jwks
+  jwks          = toset([local.jwks])
   # we set at least one role so that an authorization server is created
   # the authorization server is required for creating a service account
   rbac_role_mapping = merge({
