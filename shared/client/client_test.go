9d746a704814357f29fdad2eaf66532f47b4899d
Hayden Spitzley <hspitzley@chanzuckerberg.com>
2022-12-20 14:45:43 -0700

    chore: add tests for shared module

---
+++ shared/client/client_test.go
@@ -0,0 +1,29 @@
+package client
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type TestTokenProvider struct{}
+
+func (t TestTokenProvider) GetToken() (string, error) {
+	return "test-token", nil
+}
+
+func TestAddAuthSuccess(t *testing.T) {
+	r := require.New(t)
+	client := NewHappyClient("test", "0.0.0", "https://fake.hapi.io", TestTokenProvider{})
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s%s", client.apiBaseUrl, "/test"), nil)
+	r.NoError(err)
+
+	client.addAuth(req)
+	authHeader, ok := req.Header["Authorization"]
+
+	r.Equal(ok, true)
+	r.Equal(authHeader, []string{"Bearer test-token"})
+}
