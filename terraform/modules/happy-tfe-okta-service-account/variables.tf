fbf1c220d7d02332bc522fd9887f98863fd6f7bd
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2022-12-22 11:42:23 -0800

    feat: service account for terraform provider credentials (#894)
    * feat: service account for terraform provider credentials
    
    * commit from ci -- ran terraform-docs and pushed
    
    * remove for loops
    
    * plural
    
    * commit from ci -- ran terraform-docs and pushed
    
    * commit from ci -- ran terraform fmt and pushed
    
    * commit from ci -- ran terraform-docs and pushed
    
    * commit from ci -- ran terraform-docs and pushed
    
    * generate private key
    
    * commit from ci -- ran terraform-docs and pushed
    
    * commit from ci -- ran terraform fmt and pushed
    
    * jwks local
    
    * updated variables
    
    * env in label
    
    * commit from ci -- ran terraform-docs and pushed
    
    * commit from ci -- ran terraform fmt and pushed
    
    * docs and output
    
    * commit from ci -- ran terraform-docs and pushed
    
    * commit from ci -- ran terraform fmt and pushed
    
    * Update main.tf
    
    * commit from ci -- ran terraform-docs and pushed
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: czi-github-helper[bot] <czi-github-helper[bot]@users.noreply.github.com>
    Co-authored-by: jakeyheath <jakeyheath@users.noreply.github.com>

---
+++ terraform/modules/happy-tfe-okta-service-account/variables.tf
@@ -0,0 +1,37 @@
+variable "aws_ssm_paths" {
+  type = object({
+    client_id     = string
+    client_secret = string
+    okta_idp_url  = string
+    config_uri    = string
+  })
+  default = {
+    client_id     = "oauth2_proxy_client_id"
+    client_secret = "oauth2_proxy_client_secret"
+    okta_idp_url  = "oauth2_proxy_oidc_issuer_url"
+    config_uri    = "oauth2_proxy_config_uri"
+  }
+  description = "The name of the SSM paths for the client ID, secret, and other values produced from this app."
+}
+
+variable "app_name" {
+  type        = string
+  description = "The name of the happy application"
+}
+
+variable "service_name" {
+  type        = string
+  default     = "happy"
+  description = "The component name this service is going to be deployed into"
+}
+
+variable "env" {
+  type        = string
+  description = "The environment of this happy application"
+}
+
+variable "rbac_role_mapping" {
+  type        = map(list(string))
+  default     = {}
+  description = "The roles that will be created as claims to access tokens for users authenticating to this application"
+}
