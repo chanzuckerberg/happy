1e99c67478d6138c497e4226d3ab0b1866a6a685
Jake Heath <jheath@chanzuckerberg.com>
2022-12-23 16:09:24 -0800

    okta provider

--- terraform/modules/happy-env-ecs/versions.tf
+++ terraform/modules/happy-env-ecs/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    okta = {
+      source  = "chanzuckerberg/okta"
+      version = "~> 3.10"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.45"
