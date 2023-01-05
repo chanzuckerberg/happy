4b6873cd5b7d6e1efa9c6dbaa960ff7d20c67a27
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2023-01-05 09:26:30 -0800

    fix: revert main changes from proxy (#931)
    * fix: revert main changes from proxy
    
    * commit from ci -- ran terraform-docs and pushed
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/modules/happy-env-eks/proxy.tf
+++ terraform/modules/happy-env-eks/proxy.tf
@@ -24,7 +24,7 @@ resource "aws_route53_record" "happy_prefixed" {
 }
 
 module "proxy" {
-  source = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/eks-multi-domain-oauth-proxy?ref=main"
+  source = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/eks-multi-domain-oauth-proxy?ref=v0.236.0"
 
   tags      = var.tags
   eks       = var.eks-cluster
