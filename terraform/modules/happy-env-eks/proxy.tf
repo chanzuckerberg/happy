7ed47016ee80f9b03a7000e38f014545eb06abff
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-01-09 11:26:35 -0500

    Update proxy.tf

--- terraform/modules/happy-env-eks/proxy.tf
+++ terraform/modules/happy-env-eks/proxy.tf
@@ -41,7 +41,7 @@ module "proxy" {
   extra_proxy_args     = var.extra_proxy_args
 }
 
-resource "kubernetes_configmap" "bypass_paths"{
+resource "kubernetes_config_map" "bypass_paths"{
   metadata {
     name      = "bypass_paths"
     namespace = kubernetes_namespace.happy.metadata[0].name
