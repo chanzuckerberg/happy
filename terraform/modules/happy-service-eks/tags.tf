53645c119d804f949640f5e9f434cb24a709ba0e
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-01-24 10:19:46 -0500

    Update tags.tf

--- terraform/modules/happy-service-eks/tags.tf
+++ terraform/modules/happy-service-eks/tags.tf
@@ -2,10 +2,10 @@ locals {
   tags = {
     happy_env          = var.deployment_stage,
     happy_stack_name   = var.stack_name,
-    #happy_service_name = var.routing.service_name,
+    happy_service_name = "", #var.routing.service_name,
     happy_region       = data.aws_region.current.name,
-    #happy_image        = var.image,
-    #happy_service_type = var.service_type,
+    happy_image        = "", #var.image,
+    happy_service_type = "", #var.service_type,
     happy_last_applied = timestamp(),
   }
 }
