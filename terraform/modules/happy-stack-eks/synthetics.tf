5f79261f6da0f1e30d555f31576cbbbd01220d19
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-01-24 13:19:57 -0500

    Update synthetics.tf

--- terraform/modules/happy-stack-eks/synthetics.tf
+++ terraform/modules/happy-stack-eks/synthetics.tf
@@ -3,7 +3,7 @@ locals {
   # if this is a non-prod OIDC protected stack, make sure to allow the health check endpoint
   # through the OIDC proxy.
   synthetics = { for k, v in local.service_definitions : v.service_name =>
-    var.routing_method == "DOMAIN" ? "https://${var.service_name}.${local.external_dns}${v.health_check_path}" : "https://${v.stack_name}.${local.external_dns}${v.health_check_path}"
+    var.routing_method == "DOMAIN" ? "https://${v.service_name}.${local.external_dns}${v.health_check_path}" : "https://${var.stack_name}.${local.external_dns}${v.health_check_path}"
     if v.synthetics && (v.service_type == "EXTERNAL" || v.service_type == "INTERNAL")
   }
   opsgenie_owner = "${local.secret["tags"]["project"]}-${local.secret["tags"]["env"]}-${local.secret["tags"]["service"]}"
