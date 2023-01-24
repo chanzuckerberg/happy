e8ce85ac15606515dc87a5e9257e422159dfe3b9
Jake Heath <jheath@chanzuckerberg.com>
2023-01-24 15:41:16 -0800

    feat: add synthetics to ecs stacks module

--- terraform/modules/happy-stack-ecs/versions.tf
+++ terraform/modules/happy-stack-ecs/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.45"
     }
+    datadog = {
+      source  = "datadog/datadog"
+      version = ">= 3.20.0"
+    }
   }
-  required_version = ">= 1.0"
+  required_version = ">= 1.3"
 }
