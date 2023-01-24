1eac6831b709ac9cbb3906b93b5bf4ec87f3d72a
Jake Heath <jheath@chanzuckerberg.com>
2023-01-23 16:23:45 -0800

    unused locals

--- terraform/modules/happy-env-eks/opsgenie.tf
+++ terraform/modules/happy-env-eks/opsgenie.tf
@@ -1,16 +1,6 @@
-locals {
-  opsgenie_tags = [
-    var.tags.env,
-    var.tags.service,
-    var.tags.project,
-    var.tags.owner
-  ]
-  opsgenie_dd_service = "${var.tags.project}-${var.tags.env}-${var.tags.service} Infrastructure Monitoring"
-}
-
 module "ops-genie" {
   source               = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/ops-genie-service?ref=main"
-  service_name         = local.opsgenie_dd_service
+  service_name         = "${var.tags.project}-${var.tags.env}-${var.tags.service} Infrastructure Monitoring"
   api_integration_name = "${var.tags.project}-${var.tags.env}-${var.tags.service}"
   api_integration_type = "Datadog"
   datadog_service_name = "${var.tags.project}-${var.tags.env}-${var.tags.service}"
