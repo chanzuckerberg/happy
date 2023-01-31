5779814d3bb62969fc1e9be1f067f353bb9dc694
Jake Heath <jheath@chanzuckerberg.com>
2023-01-31 14:16:24 -0800

    okta groups not okta group

--- terraform/modules/happy-tfe-okta-app/main.tf
+++ terraform/modules/happy-tfe-okta-app/main.tf
@@ -31,14 +31,16 @@ module "happy_app" {
 resource "okta_app_group_assignments" "happy_app" {
   app_id = module.happy_app.app.id
   dynamic "group" {
-    for_each = toset([for k, v in data.okta_group.teams : v.id])
+    for_each = toset(flatten([
+      for v in data.okta_group.teams.groups : [for x, y in v: y.id]
+    ]))
     content {
       id = group.value
     }
   }
 }
 
-data "okta_group" "teams" {
+data "okta_groups" "teams" {
   for_each = var.teams
-  name     = each.value
+   q     = each.value
 }
