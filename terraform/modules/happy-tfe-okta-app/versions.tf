d059643eb70a20938d3eaa3a1424ae50ce2fb87a
Jake Heath <jheath@chanzuckerberg.com>
2023-01-31 11:44:36 -0800

    remove AWS version requirement

--- terraform/modules/happy-tfe-okta-app/versions.tf
+++ terraform/modules/happy-tfe-okta-app/versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "okta/okta"
       version = "~> 3.41"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.45"
-    }
   }
   required_version = ">= 1.3"
 }
