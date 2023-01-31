ac6d444e62136a998480ca51f45ed8f7b97bde49
Jake Heath <jheath@chanzuckerberg.com>
2023-01-30 16:00:19 -0800

    external zone name back in

--- terraform/modules/happy-env-eks/integration_secret.tf
+++ terraform/modules/happy-env-eks/integration_secret.tf
@@ -3,6 +3,7 @@ locals {
     kind    = "k8s"
     vpc_id  = var.cloud-env.vpc_id
     zone_id = var.base_zone_id
+    external_zone_name = data.aws_route53_zone.base_zone.name
 
     cloud_env       = var.cloud-env
     eks_cluster     = var.eks-cluster
