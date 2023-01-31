12c74800ebafd3c7e7739a41c7cbf1669b4e6c20
Jake Heath <jheath@chanzuckerberg.com>
2023-01-31 11:43:44 -0800

    go

--- terraform/modules/happy-env-eks/oidc.tf
+++ terraform/modules/happy-env-eks/oidc.tf
@@ -1,5 +1,5 @@
 module "happy_okta_app" {
-  source = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-tfe-okta-app?ref=happy-tfe-okta-app-v2.0.0"
+  source = "../happy-tfe-okta-app"
 
   app_name = var.tags.project
   env      = var.tags.env
--- terraform/modules/happy-env-eks/versions.tf
+++ terraform/modules/happy-env-eks/versions.tf
@@ -12,12 +12,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.16"
     }
-    opsgenie = {
-      source = "opsgenie/opsgenie"
-      # [JH]: opsgenie terraform provider is full of bugs, this is the only version right now that doesn't throw stack traces
-      # be careful when updating this version
-      version = "= 0.6.14"
-    }
   }
   required_version = ">= 1.3"
 }
