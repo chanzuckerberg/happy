eff565747561a71efb361b1c51ab3c106aa37ba0
Jake Heath <jheath@chanzuckerberg.com>
2023-01-30 20:25:46 -0800

    only project

--- terraform/modules/happy-env-eks/oidc.tf
+++ terraform/modules/happy-env-eks/oidc.tf
@@ -1,7 +1,7 @@
 module "happy_okta_app" {
   source = "../happy-tfe-okta-app"
 
-  app_name = "${var.tags.project}-${var.tags.env}-${var.tags.service}"
+  app_name = var.tags.project
   env      = var.tags.env
   teams    = var.okta_teams
 }
