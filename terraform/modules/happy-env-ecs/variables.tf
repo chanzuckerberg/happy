646fa1d6aa5a207219bd4967db37688e0c6d950b
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-01-31 10:37:55 -0500

    Update variables.tf

--- terraform/modules/happy-env-ecs/variables.tf
+++ terraform/modules/happy-env-ecs/variables.tf
@@ -190,9 +190,3 @@ variable "authorized_github_repos" {
   type        = map(object({ repo_name : string, app_name : string }))
   default     = {}
 }
-
-variable "datadog_api_key" {
-  type        = string
-  default     = ""
-  description = "Datadog API key for the Datadog agent"
-}
