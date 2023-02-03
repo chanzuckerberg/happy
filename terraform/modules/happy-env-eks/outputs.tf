969075c0fbf8baabb833ff108f5c1efffd76e4a8
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-02-03 10:44:19 -0500

    feat: Sample Happy Environment EKS Datadog dashboard

--- terraform/modules/happy-env-eks/outputs.tf
+++ terraform/modules/happy-env-eks/outputs.tf
@@ -1,3 +1,11 @@
 output "namespace" {
   value = kubernetes_namespace.happy.id
 }
+
+
+output "dashboard" {
+  value = {
+    id = datadog_dashboard_json.environment_dashboard.id
+    url = datadog_dashboard_json.environment_dashboard.url
+  }
+}
