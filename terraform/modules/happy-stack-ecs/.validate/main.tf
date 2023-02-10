26da9201dc0351ced0b64d3c3dd4c35e02bc3a3a
Hayden Spitzley <105455169+hspitzley-czi@users.noreply.github.com>
2023-02-10 11:18:59 -0500

    chore: more advanced tf validation (#1131)

---
+++ terraform/modules/happy-stack-ecs/.validate/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+}
+
+terraform {
+  required_version = ">=1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.45.0"
+    }
+  }
+}
+
+module "test" {
+  source = "../"
+
+  app_name              = "testapp"
+  happy_config_secret   = "happy_config_secret"
+  image_tag             = "latest"
+  priority              = 1
+  stack_name            = "my-stack"
+  deployment_stage      = "rdev"
+  require_okta          = true
+  stack_prefix          = "/my-stack"
+  wait_for_steady_state = true
+  chamber_service       = "happy-rdev-testapp"
+  service_port          = 3001
+}
