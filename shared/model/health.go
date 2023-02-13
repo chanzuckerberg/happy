cf8b0175d6367e05146b0ba6359655d9fdb14e5a
Hayden Spitzley <105455169+hspitzley-czi@users.noreply.github.com>
2023-02-13 15:12:43 -0500

    feat: inject release version into api image (#1139)

--- shared/model/health.go
+++ shared/model/health.go
@@ -2,6 +2,8 @@ package model
 
 // @Description Health info
 type HealthResponse struct {
-	Status string `json:"status" example:"OK"`
-	Route  string `json:"route" example:"/"`
+	Status  string `json:"status" example:"OK"`
+	Route   string `json:"route" example:"/"`
+	Version string `json:"version" example:"v0.50.2"`
+	GitSha  string `json:"git_sha" example:"b746df8"`
 } // @Name response.Health
