026737da67ccbfd65679b1e5167d07326813e028
Jake Heath <jheath@chanzuckerberg.com>
2023-02-15 15:21:20 -0800

    remove commented out section

--- terraform/modules/happy-stack-eks/happy_config.tf
+++ terraform/modules/happy-stack-eks/happy_config.tf
@@ -1,4 +1,4 @@
-/*data "happy_resolved_app_configs" "configs" {
+data "happy_resolved_app_configs" "configs" {
   app_name    = length(var.app_name) == 0 ? local.secret["tags"]["project"] : var.app_name
   environment = var.deployment_stage
   stack       = var.stack_name
@@ -15,4 +15,4 @@ provider "happy" {
   api_kms_key_id      = local.secret["hapi_config"]["kms_key_id"]
   api_assume_role_arn = local.secret["hapi_config"]["assume_role_arn"]
   api_oidc_scope      = local.secret["hapi_config"]["scope"]
-}*/
+}
