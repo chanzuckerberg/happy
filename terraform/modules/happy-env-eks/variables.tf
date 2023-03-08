2c02ef04381dc851d848121b55a898b849d0b36a
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-03-08 16:48:03 -0500

    Update variables.tf

--- terraform/modules/happy-env-eks/variables.tf
+++ terraform/modules/happy-env-eks/variables.tf
@@ -28,11 +28,17 @@ variable "ecr_repos" {
 variable "rds_dbs" {
   description = "Map of DB's to create for your happy applications. If an engine_version is not provided, the default_db_engine_version is used"
   type = map(object({
-    name                   = string,
-    username               = string,
-    instance_class         = string,
-    engine_version         = string,
-    rds_cluster_parameters = list(any),
+    name           = string,
+    username       = string,
+    instance_class = string,
+    engine_version = string,
+    rds_cluster_parameters = list(object(
+      {
+        name         = string
+        value        = string
+        apply_method = string
+      }
+    )),
   }))
   default = {}
 }
