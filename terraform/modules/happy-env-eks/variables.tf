0c5d952691394c8ad3be5fe4fcf41b2a9d7f0da5
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-03-08 16:40:48 -0500

    Update variables.tf

--- terraform/modules/happy-env-eks/variables.tf
+++ terraform/modules/happy-env-eks/variables.tf
@@ -32,7 +32,7 @@ variable "rds_dbs" {
     username       = string,
     instance_class = string,
     engine_version = string,
-    rds_cluster_parameters = optional(list(map(string)), []),
+    rds_cluster_parameters = list(any),
   }))
   default = {}
 }
