782a650aa6366d7b8f27d94642c0bb21fd99c10c
Hayden Spitzley <105455169+hspitzley-czi@users.noreply.github.com>
2023-03-14 16:27:34 -0500

    feat: automate adding OIDC providers for new happy apps to happy api (#1353)

---
+++ terraform/modules/happy-tfe-okta-service-account/ssm_writer.tf
@@ -0,0 +1,16 @@
+# write the oidc provider values that happy api needs to the hapi chamber service so they will be available to happy api
+locals {
+  param_suffix = replace("${var.tags.service}_${var.tags.env}_${var.tags.project}", "-", "_")
+}
+
+module "params" {
+  source  = "github.com/chanzuckerberg/cztack//aws-ssm-params-writer?ref=v0.43.1"
+  service = "hapi"
+  project = "happy"
+  env     = var.tags.env
+  owner   = var.tags.owner
+
+  parameters = {
+    "oidc_provider_${local.param_suffix}" : "${okta_auth_server.auth_server.issuer}|${local.label}"
+  }
+}
