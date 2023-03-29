e285ba09147e39fde7041defbcf17548ba6dd2f3
David Spadea <dspadea@chanzuckerberg.com>
2023-03-29 15:31:28 -0400

    Skip Path field in serialization

--- cli/pkg/config/happy_version_lock.go
+++ cli/pkg/config/happy_version_lock.go
@@ -12,7 +12,7 @@ import (
 
 type HappyVersionLockFile struct {
 	HappyVersion string
-	Path         string
+	Path         string `json:"-"`
 }
 
 func NewHappyVersionLockFile(projectRoot string, requiredVersion string) (*HappyVersionLockFile, error) {
