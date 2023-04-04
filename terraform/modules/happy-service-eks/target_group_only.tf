fcd724531cc48f66f4cdcb768fb1cbfb684c290b
Jake Heath <jheath@chanzuckerberg.com>
2023-04-04 10:33:42 -0700

    healthcheck

--- terraform/modules/happy-service-eks/target_group_only.tf
+++ terraform/modules/happy-service-eks/target_group_only.tf
@@ -11,8 +11,8 @@ resource "aws_lb_target_group" "this" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.cloud_env.vpc_id
-  health_check = {
-    path = "/health"
+  health_check {
+    path = var.health_check_path
   }
 }
 
