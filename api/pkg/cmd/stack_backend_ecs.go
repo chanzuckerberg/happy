696107ee87789cccd6c38f3160270be0d1dbc3e8
Hayden Spitzley <hspitzley@chanzuckerberg.com>
2023-04-06 13:25:27 -0400

    use EnvironmentContext for ecs

--- api/pkg/cmd/stack_backend_ecs.go
+++ api/pkg/cmd/stack_backend_ecs.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -10,8 +9,10 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/chanzuckerberg/happy/api/pkg/request"
+	compute_backend "github.com/chanzuckerberg/happy/shared/backend/aws"
 	"github.com/chanzuckerberg/happy/shared/config"
 	"github.com/chanzuckerberg/happy/shared/model"
+	"github.com/chanzuckerberg/happy/shared/util"
 	"github.com/pkg/errors"
 )
 
@@ -43,18 +44,25 @@ func (s *StackBackendECS) GetAppStacks(ctx context.Context, payload model.AppSta
 		return nil, errors.Wrap(err, "could not get parameter")
 	}
 
-	secretsClient := getSecretsManagerClient(ctx, payload)
-	out, err := secretsClient.GetSecretValue(ctx, &secretsmanager.GetSecretValueInput{
-		SecretId: &payload.SecretId,
-	})
+	envCtx := config.EnvironmentContext{
+		EnvironmentName: payload.Environment,
+		AWSProfile:      &payload.AwsProfile,
+		AWSRegion:       &payload.AwsRegion,
+		SecretId:        payload.SecretId,
+		TaskLaunchType:  util.LaunchType(payload.TaskLaunchType),
+	}
+	b, err := compute_backend.NewAWSBackend(ctx, envCtx)
 	if err != nil {
-		return nil, errors.Wrapf(err, "could not get integration secret at %s", payload.SecretId)
+		return nil, errors.Wrap(err, "could not create backend")
 	}
 
-	secret := &config.IntegrationSecret{}
-	err = json.Unmarshal([]byte(*out.SecretString), secret)
+	computeBackend := compute_backend.ECSComputeBackend{
+		Backend:  b,
+		SecretId: payload.SecretId,
+	}
+	integrationSecret, _, err := computeBackend.GetIntegrationSecret(ctx)
 	if err != nil {
-		return nil, errors.Wrap(err, "could not json parse integraiton secret")
+		return nil, err
 	}
 
 	stacklist, err := parseParamToStacklist(*result.Parameter.Value)
@@ -62,5 +70,5 @@ func (s *StackBackendECS) GetAppStacks(ctx context.Context, payload model.AppSta
 		return nil, errors.Wrap(err, "could not parse json")
 	}
 
-	return enrichStacklistMetadata(ctx, stacklist, payload, secret)
+	return enrichStacklistMetadata(ctx, stacklist, payload, integrationSecret)
 }
