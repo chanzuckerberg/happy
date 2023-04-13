236a6e8b129ce4276714442f9c191cd9b32519a7
Annie Ku <anne.ku@chanzuckerberg.com>
2023-04-13 16:48:45 -0700

    fix: remove WAF configuration in happy-env-ecs

--- terraform/modules/happy-env-eks/waf.tf
+++ terraform/modules/happy-env-eks/waf.tf
@@ -5,7 +5,7 @@ locals {
 # Only create a WAF if we decide to via variables
 module "regional-waf" {
   count  = var.include_waf ? 1 : 0
-  source = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/web-acl-regional?ref=web-acl-regional-v1.1.0"
+  source = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/web-acl-regional?ref=web-acl-regional-v1.2.4"
   tags   = var.tags
   name   = local.web_acl_name
 }
