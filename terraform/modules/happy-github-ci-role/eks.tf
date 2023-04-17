554805bd954e7be9bbcf65a07c3e6fbd7b5aef92
Jake Heath <jheath@chanzuckerberg.com>
2023-04-17 14:53:47 -0700

    arn

--- terraform/modules/happy-github-ci-role/eks.tf
+++ terraform/modules/happy-github-ci-role/eks.tf
@@ -1,7 +1,7 @@
 module "eks_cluster_permissions" {
   count           = var.eks_cluster_arn != "" ? 1 : 0
   source          = "../happy-github-ci-role-eks"
-  eks_cluster_arn = module.eks_cluster.arn
+  eks_cluster_arn = var.eks_cluster_arn
   gh_actions_role = module.gh_actions_role
 
   depends_on = [
