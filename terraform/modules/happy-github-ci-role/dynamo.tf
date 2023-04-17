d230a82cbb3dba40150f94e12648a1bd914828a7
Jake Heath <jheath@chanzuckerberg.com>
2023-04-17 11:15:57 -0700

    optional dynamo

--- terraform/modules/happy-github-ci-role/dynamo.tf
+++ terraform/modules/happy-github-ci-role/dynamo.tf
@@ -1,4 +1,5 @@
 module "dynamodb_writer" {
+  count     = var.dynamodb_table_arn != "" ? 1 : 0
   source    = "git@github.com:chanzuckerberg/shared-infra//terraform/modules/aws-iam-policy-dynamodb-rw?ref=v0.290.3"
   table_arn = var.dynamodb_table_arn
   role_name = module.gh_actions_role.role.name
