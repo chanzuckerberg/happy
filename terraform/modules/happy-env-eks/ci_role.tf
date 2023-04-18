6703a44083e968fef8a35732aca59d11199b1899
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2023-04-18 14:31:42 -0700

    feat: enable EKS cluster permissions for CI role

--- terraform/modules/happy-env-eks/ci_role.tf
+++ terraform/modules/happy-env-eks/ci_role.tf
@@ -5,6 +5,6 @@ module "happy_github_ci_role" {
   ecr_repo_arns           = flatten([for ecr in module.ecrs : ecr.repository_arn])
   authorized_github_repos = [each.value.repo_name]
   happy_app_name          = each.value.app_name
-
+  eks_cluster_arn = var.eks-cluster.cluster_arn
   tags = var.tags
 }
