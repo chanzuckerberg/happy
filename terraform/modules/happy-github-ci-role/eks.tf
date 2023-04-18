a856f6ce50b661e227db7d26e4943f82da37bab0
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2023-04-18 08:28:54 -0700

    fix: the CI role with latest permissions (#1497)
    * fix: the CI role with latest permissions
    
    * commit from ci -- ran terraform-docs and pushed
    
    * commit from ci -- ran terraform fmt and pushed
    
    * chore: separte into module
    
    * commit from ci -- ran terraform-docs and pushed
    
    * optional dynamo
    
    * commit from ci -- ran terraform-docs and pushed
    
    * name of module
    
    * docs
    
    * commit from ci -- ran terraform-docs and pushed
    
    * random pet
    
    * commit from ci -- ran terraform-docs and pushed
    
    * terraform versions
    
    * commit from ci -- ran terraform-docs and pushed
    
    * arn
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: czi-github-helper[bot] <czi-github-helper[bot]@users.noreply.github.com>

---
+++ terraform/modules/happy-github-ci-role/eks.tf
@@ -0,0 +1,10 @@
+module "eks_cluster_permissions" {
+  count           = var.eks_cluster_arn != "" ? 1 : 0
+  source          = "../happy-github-ci-role-eks"
+  eks_cluster_arn = var.eks_cluster_arn
+  gh_actions_role = module.gh_actions_role
+
+  depends_on = [
+    module.gh_actions_role
+  ]
+}
