fc9324de134fe5ab6ca7e61c8d7c7d136b794985
Jake Heath <jheath@chanzuckerberg.com>
2023-04-24 15:35:48 -0700

    add staging/prod to typical

--- examples/typical_app/.happy/terraform/envs/prod/main.tf
+++ examples/typical_app/.happy/terraform/envs/prod/main.tf
@@ -4,7 +4,7 @@ module "stack" {
   image_tag        = var.image_tag
   image_tags       = jsondecode(var.image_tags)
   stack_name       = var.stack_name
-  deployment_stage = "rdev"
+  deployment_stage = "prod"
 
   stack_prefix  = "/${var.stack_name}"
   k8s_namespace = var.k8s_namespace
