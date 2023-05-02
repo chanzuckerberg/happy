1b2e12ac382ff617b65de3fa7f80c7095b40f598
Jake Heath <jheath@chanzuckerberg.com>
2023-05-02 14:16:57 -0700

    versions

--- terraform/modules/happy-github-ci-role/versions.tf
+++ terraform/modules/happy-github-ci-role/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.45"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.1"
+    }
   }
   required_version = ">= 1.0"
 }
