adb8dbc78a38d65016bd0b13bf18d70f5c1f478f
Jake Heath <jheath@chanzuckerberg.com>
2023-05-02 14:08:09 -0700

    for_eachs

--- terraform/modules/happy-env-eks/ci_role.tf
+++ terraform/modules/happy-env-eks/ci_role.tf
@@ -1,9 +1,9 @@
 module "happy_github_ci_role" {
-  for_each = var.github_actions_roles
+  for_each = toset([for role in var.github_actions_roles: role.name])
   source   = "../happy-github-ci-role"
 
   ecrs                 = module.ecrs
-  gh_actions_role_name = each.value.name
+  gh_actions_role_name = each.value
   eks_cluster_arn      = var.eks-cluster.cluster_arn
   tags                 = var.tags
 }
