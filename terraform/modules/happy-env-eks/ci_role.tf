8110d199aa75485482e394c344b333cca9871f3d
Jake Heath <jheath@chanzuckerberg.com>
2023-05-02 13:47:48 -0700

    string

--- terraform/modules/happy-env-eks/ci_role.tf
+++ terraform/modules/happy-env-eks/ci_role.tf
@@ -3,7 +3,7 @@ module "happy_github_ci_role" {
   source   = "../happy-github-ci-role"
 
   ecrs                 = module.ecrs
-  gh_actions_role_name = [each.value.name]
+  gh_actions_role_name = each.value.name
   eks_cluster_arn      = var.eks-cluster.cluster_arn
   tags                 = var.tags
 }
