056180253168b278c945aded40e68141ab75f566
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-05-03 16:50:46 -0400

    Per code review.

--- shared/util/cloner.go
+++ shared/util/cloner.go
@@ -5,7 +5,7 @@ import (
 	"reflect"
 )
 
-func DeepClone(dst, src interface{}) error {
+func DeepClone(dst, src any) error {
 	data, err := json.Marshal(src)
 	if err != nil {
 		return err
@@ -17,16 +17,16 @@ func DeepClone(dst, src interface{}) error {
 	return nil
 }
 
-func DeepMerge(dst, src map[string]interface{}) {
+func DeepMerge(dst, src map[string]any) {
 	for k, v := range src {
 		t := reflect.TypeOf(v)
 		if t == nil {
 			continue
 		}
 		switch v.(type) {
-		case map[string]interface{}:
-			subSrc := v.(map[string]interface{})
-			subDst := dst[k].(map[string]interface{})
+		case map[string]any:
+			subSrc := v.(map[string]any)
+			subDst := dst[k].(map[string]any)
 			DeepMerge(subDst, subSrc)
 			dst[k] = subDst
 		default:
