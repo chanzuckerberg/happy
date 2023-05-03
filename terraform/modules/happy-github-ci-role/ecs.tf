88d09d6d04ce496d08f55baecf81309f7493e4d2
Jake Heath <jheath@chanzuckerberg.com>
2023-05-02 17:47:29 -0700

    fix: wrong count argument

--- terraform/modules/happy-github-ci-role/ecs.tf
+++ terraform/modules/happy-github-ci-role/ecs.tf
@@ -1,5 +1,5 @@
 module "ecs_cluster_permissions" {
-  count  = length(var.ecs.arn) == "" ? 0 : 1
+  count  = length(var.ecs.arn) == 0 ? 0 : 1
   source = "../happy-github-ci-role-ecs"
   #  ecs_cluster_arn      = var.ecs.cluster_arn TODO: not needed yet
   env                  = var.tags.env
