61afbdac2796d213b2b722c5e4f42044e00cfe48
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2023-05-02 17:35:41 -0700

    fix: dynamo alphanumeric sid (#1687)
    * fix: dynamo alphanumeric sid
    
    * Update README.md
    
    * commit from ci -- ran terraform-docs and pushed
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/modules/happy-github-ci-role/dynamo.tf
+++ terraform/modules/happy-github-ci-role/dynamo.tf
@@ -1,6 +1,6 @@
 module "dynamodb_writer" {
   count     = var.dynamodb_table_arn != "" ? 1 : 0
-  source    = "git@github.com:chanzuckerberg/cztack//aws-iam-policy-dynamodb-rw?ref=v0.55.0"
+  source    = "git@github.com:chanzuckerberg/cztack//aws-iam-policy-dynamodb-rw?ref=v0.55.1"
   table_arn = var.dynamodb_table_arn
   role_name = var.gh_actions_role_name
   tags      = var.tags
