4ef06f722ba8342d42cafc1482c11b1d55c4a8bd
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-05-31 12:57:52 -0400

    Update hcl_manager.go

--- shared/hclmanager/hcl_manager.go
+++ shared/hclmanager/hcl_manager.go
@@ -127,7 +127,7 @@ func (h HclManager) Ingest(ctx context.Context) error {
 		parser := tf.NewTfParser()
 		moduleCall, err := parser.ParseModuleCall(srcDir)
 		if err != nil {
-			return errors.Wrapf(err, "Unable to parse a stack module call for environment %s", environment)
+			return errors.Wrapf(err, "Unable to parse a stack module call for environment %s", name)
 		}
 
 		moduleCall.Parameters = util.DeepCleanup(moduleCall.Parameters)
@@ -153,7 +153,7 @@ func (h HclManager) Ingest(ctx context.Context) error {
 }
 
 func (h HclManager) Validate(ctx context.Context) error {
-	for _, environment := range h.HappyConfig.GetData().Environments {
+	for name, environment := range h.HappyConfig.GetData().Environments {
 		tfDirPath := environment.TerraformDirectory
 
 		happyProjectRoot := h.HappyConfig.GetProjectRoot()
@@ -162,7 +162,7 @@ func (h HclManager) Validate(ctx context.Context) error {
 		parser := tf.NewTfParser()
 		_, err := parser.ParseModuleCall(srcDir)
 		if err != nil {
-			return errors.Wrapf(err, "Unable to parse a stack module call for environment %s", environment)
+			return errors.Wrapf(err, "Unable to parse a stack module call for environment %s", name)
 		}
 	}
 	return nil
