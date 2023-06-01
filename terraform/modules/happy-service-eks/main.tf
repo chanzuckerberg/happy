d60c1607d114a8e75349d0289a6797e5feee27d9
Alex Biju <abiju@chanzuckerberg.com>
2023-06-01 00:39:56 -0500

    check annotations

--- terraform/modules/happy-service-eks/main.tf
+++ terraform/modules/happy-service-eks/main.tf
@@ -68,6 +68,7 @@ resource "kubernetes_deployment_v1" "deployment" {
             "managedby"        = "happy"
             "happy_compute"    = "eks"
           })
+          "linkerd.io/inject": "enabled"
         }
       }
 
