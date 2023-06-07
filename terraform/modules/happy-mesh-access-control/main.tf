a90549034aa14bbc8ef05e91c58ea034d66b8f67
abiju-czi <abiju-czi@users.noreply.github.com>
2023-06-07 04:21:39 +0000

    commit from ci -- ran terraform fmt and pushed

--- terraform/modules/happy-mesh-access-control/main.tf
+++ terraform/modules/happy-mesh-access-control/main.tf
@@ -35,8 +35,8 @@ resource "kubernetes_manifest" "linkerd_mesh_tls_authentication" {
     }
     "spec" = {
       "identityRefs" = concat([for v in var.allow_mesh_services : {
-        "kind" = "ServiceAccount"
-        "name" = "${v.stack}-${v.service}-${var.deployment_stage}-${v.stack}"
+        "kind"      = "ServiceAccount"
+        "name"      = "${v.stack}-${v.service}-${var.deployment_stage}-${v.stack}"
         "namespace" = var.k8s_namespace
         }], local.allow_ingress_controller ? [{
         "kind"      = "ServiceAccount"
