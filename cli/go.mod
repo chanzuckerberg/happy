267cf5d5a172e6f0235681930e58b12ed41bec5c
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-06-08 14:59:30 -0400

    Added validation and changed the order of questions

--- cli/go.mod
+++ cli/go.mod
@@ -38,6 +38,7 @@ require (
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/aws/aws-sdk-go v1.44.269 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.13.24 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.10.25 // indirect
--- cli/pkg/config_manager/config_manager.go
+++ cli/pkg/config_manager/config_manager.go
@@ -6,9 +6,14 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"reflect"
+	"regexp"
+	"sort"
+	"strconv"
 	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
+	"github.com/asaskevich/govalidator"
 	backend "github.com/chanzuckerberg/happy/shared/backend/aws"
 	"github.com/chanzuckerberg/happy/shared/config"
 	"github.com/chanzuckerberg/happy/shared/k8s"
@@ -22,6 +27,9 @@ type Service struct {
 	ServiceType    string
 	Context        string
 	DockerfilePath string
+	Port           int
+	Uri            string
+	Priority       int
 }
 
 func CreeateHappyConfig(ctx context.Context, bootstrapConfig *config.Bootstrap) (*config.HappyConfig, error) {
@@ -39,57 +47,19 @@ func CreeateHappyConfig(ctx context.Context, bootstrapConfig *config.Bootstrap) 
 		return nil, errors.New("no dockerfiles found in this repo")
 	}
 
-	services := []Service{}
-	logrus.Info("We have found dockerfiles in your project, let's see if you'd like to use them as services in your stack")
-	for _, dockerPath := range dockerPaths {
-		dockerFileName := filepath.Base(dockerPath)
-		contextPath, err := filepath.Rel(bootstrapConfig.HappyProjectRoot, filepath.Dir(dockerPath))
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to obtain relative path")
-		}
+	logrus.Info("Welcome to happy bootstrap! We'll ask you a few questions to get started.")
 
-		confirm := false
-		prompt := &survey.Confirm{
-			Message: fmt.Sprintf("Would you like to use %s as a service in your stack?", filepath.Join(contextPath, dockerFileName)),
-		}
-		err = survey.AskOne(prompt, &confirm)
-		if err != nil {
-			return nil, errors.Wrap(err, "unable to prompt")
-		}
-		if !confirm {
-			continue
-		}
-
-		serviceName := ""
-		prompt1 := &survey.Input{
-			Message: fmt.Sprintf("What would you like to name the service for %s?", contextPath),
-			Help:    "This will be the name of the service in your stack, lowercased and hyphenated",
-			Default: "frontend",
-		}
-		err = survey.AskOne(prompt1, &serviceName)
-		if err != nil {
-			return nil, errors.Wrap(err, "unable to prompt")
-		}
-		serviceName = strings.ToLower(serviceName)
-
-		serviceType := "PRIVATE"
-		prompt2 := &survey.Select{
-			Message: fmt.Sprintf("What kind of service is %s?", serviceName),
-			Options: []string{"PRIVATE", "PUBLIC", "EXTERNAL"},
-			Default: serviceType,
-		}
-
-		err = survey.AskOne(prompt2, &serviceType)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to obtain an aws profile")
-		}
-
-		services = append(services, Service{
-			Name:           serviceName,
-			ServiceType:    serviceType,
-			DockerfilePath: dockerFileName,
-			Context:        contextPath,
-		})
+	appName := ""
+	prompt1 := &survey.Input{
+		Message: "What would you like to name this application?",
+		Help:    "This will be the unique name of the application, lowercased and hyphenated",
+	}
+	err = survey.AskOne(prompt1, &appName, survey.WithValidator(survey.Required), survey.WithValidator(survey.MinLength(5)))
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to prompt")
+	}
+	if len(appName) == 0 {
+		return nil, errors.New("no application name provided")
 	}
 
 	profiles, err := util.GetAwsProfiles()
@@ -105,15 +75,15 @@ func CreeateHappyConfig(ctx context.Context, bootstrapConfig *config.Bootstrap) 
 		{
 			Name: "environments",
 			Prompt: &survey.MultiSelect{
-				Message: "Which environments should we create?",
-				Options: []string{"rdev", "staging", "prod"},
+				Message: "Your application will be deployed to multiple environments. Which environments would you like to deploy to?",
+				Options: []string{"rdev", "dev", "staging", "prod"},
 			},
 		},
 	}
 
 	err = survey.Ask(prompt, &environmentNames)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to obtain an eks cluster id")
+		return nil, errors.Wrapf(err, "failed to prompt")
 	}
 	if len(environmentNames) == 0 {
 		return nil, errors.New("no environments were selected")
@@ -121,78 +91,91 @@ func CreeateHappyConfig(ctx context.Context, bootstrapConfig *config.Bootstrap) 
 
 	environments := map[string]config.Environment{}
 	for _, env := range environmentNames {
-		logrus.Infof("A few questions about %s environment", env)
+		logrus.Infof("A few questions about environment  '%s':", env)
+
 		profile := ""
-		prompt := &survey.Select{
-			Message: fmt.Sprintf("Which aws profile do you want to use in %s?", env),
-			Options: profiles,
-			Default: profiles[0],
-		}
-
-		err = survey.AskOne(prompt, &profile)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to obtain an aws profile")
-		}
-
 		region := ""
-		prompt = &survey.Select{
-			Message: fmt.Sprintf("Which aws region should we use in %s?", env),
-			Options: []string{"us-west-2", "us-east-1", "us-east-2", "us-west-1"},
-			Default: "us-west-2",
-		}
-
-		err = survey.AskOne(prompt, &region)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to obtain an aws profile")
-		}
-
-		clusterIds, err := ListClusterIds(ctx, profile, region)
-		if err != nil {
-			return nil, errors.Wrap(err, "unable to list eks clusters")
-		}
-		if len(clusterIds) == 0 {
-			return nil, errors.New("no eks clusters found")
-		}
-
 		clusterId := ""
-		prompt = &survey.Select{
-			Message: fmt.Sprintf("Which EKS cluster should we use in %s?", env),
-			Options: clusterIds,
-			Default: clusterIds[0],
-		}
-
-		err = survey.AskOne(prompt, &clusterId)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to obtain an eks cluster id")
-		}
-
-		happyNamespaces, err := ListHappyNamespaces(ctx, profile, region, clusterId)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to obtain a list of happy namespaces")
-		}
-		if len(happyNamespaces) == 0 {
-			return nil, errors.New("no happy namespaces found in the cluster")
-		}
-
-		defaultNamespace := happyNamespaces[0]
-		for _, namespace := range happyNamespaces {
-			if strings.Contains(namespace, fmt.Sprintf("-%s-happy-env", env)) {
-				defaultNamespace = namespace
-				break
-			}
-		}
-
 		happyNamespace := ""
-		prompt = &survey.Select{
-			Message: fmt.Sprintf("Which happy namespace should we use in %s?", env),
-			Options: happyNamespaces,
-			Default: defaultNamespace,
+
+		for {
+			prompt := &survey.Select{
+				Message: fmt.Sprintf("Which aws profile do you want to use in %s?", env),
+				Options: profiles,
+				Default: profiles[0],
+			}
+
+			err = survey.AskOne(prompt, &profile)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to prompt")
+			}
+			if len(profile) == 0 {
+				continue
+			}
+
+			var clusterIds []string
+			prompt1 := &survey.Select{
+				Message: fmt.Sprintf("Which aws region should we use in %s? (us-west-1 should be avoided, if possible)", env),
+				Options: []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2"},
+				Default: "us-west-2",
+			}
+
+			err = survey.AskOne(prompt1, &region)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to prompt")
+			}
+			logrus.Info("Checking for eks clusters in this region...")
+
+			clusterIds, err = ListClusterIds(ctx, profile, region)
+			if err != nil {
+				return nil, errors.Wrap(err, "unable to list eks clusters")
+			}
+			if len(clusterIds) == 0 {
+				logrus.Error("No eks clusters found in this region. Please select a different region or profile.")
+				continue
+			}
+
+			prompt = &survey.Select{
+				Message: fmt.Sprintf("Which EKS cluster should we use in %s?", env),
+				Options: clusterIds,
+				Default: clusterIds[0],
+			}
+
+			err = survey.AskOne(prompt, &clusterId)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to prompt")
+			}
+			var happyNamespaces []string
+			happyNamespaces, err = ListHappyNamespaces(ctx, profile, region, clusterId)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to obtain a list of happy namespaces")
+			}
+			if len(happyNamespaces) == 0 {
+				logrus.Error("No happy namespaces were found in the selected cluster, please select a different region, profile or eks cluster.")
+				continue
+			}
+
+			defaultNamespace := happyNamespaces[0]
+			for _, namespace := range happyNamespaces {
+				if strings.Contains(namespace, fmt.Sprintf("-%s-happy-env", env)) {
+					defaultNamespace = namespace
+					break
+				}
+			}
+
+			prompt2 := &survey.Select{
+				Message: fmt.Sprintf("Which happy namespace should we use in %s?", env),
+				Options: happyNamespaces,
+				Default: defaultNamespace,
+			}
+
+			err = survey.AskOne(prompt2, &happyNamespace)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to obtain an eks cluster id")
+			}
+			break
 		}
 
-		err = survey.AskOne(prompt, &happyNamespace)
-		if err != nil {
-			return nil, errors.Wrapf(err, "failed to obtain an eks cluster id")
-		}
 		environments[env] = config.Environment{
 			AWSProfile: util.String(profile),
 			AWSRegion:  util.String(region),
@@ -206,6 +189,96 @@ func CreeateHappyConfig(ctx context.Context, bootstrapConfig *config.Bootstrap) 
 			TaskLaunchType:     "k8s",
 		}
 	}
+
+	services := []Service{}
+	logrus.Info("We have found dockerfiles in your project, let's see if you'd like to use them as services in your stack")
+	for _, dockerPath := range dockerPaths {
+		dockerFileName := filepath.Base(dockerPath)
+		contextPath, err := filepath.Rel(bootstrapConfig.HappyProjectRoot, filepath.Dir(dockerPath))
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to obtain relative path")
+		}
+
+		confirm := false
+		prompt := &survey.Confirm{
+			Message: fmt.Sprintf("Would you like to use dockerfile %s/%s as a service in your stack?", contextPath, dockerFileName),
+		}
+		err = survey.AskOne(prompt, &confirm)
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to prompt")
+		}
+		if !confirm {
+			continue
+		}
+
+		serviceName := ""
+		prompt1 := &survey.Input{
+			Message: fmt.Sprintf("What would you like to name the service for %s?", contextPath),
+			Help:    "This will be the name of the service in your stack, lowercased and hyphenated",
+			Default: "frontend",
+		}
+		err = survey.AskOne(prompt1, &serviceName, survey.WithValidator(survey.Required), survey.WithValidator(survey.MinLength(3)))
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to prompt")
+		}
+		serviceName = strings.ToLower(serviceName)
+
+		serviceType := "Service is exposed to the internet, and can only be consumed by other services in the stack (PRIVATE)"
+		prompt2 := &survey.Select{
+			Message: fmt.Sprintf("What kind of service is %s?", serviceName),
+			Options: []string{
+				"Serivce is exposed to the internet (EXTERNAL)",
+				"Service is exposed to the internet, but is protected by Okta (INTERNAL)",
+				"Service is exposed to the internet, and can only be consumed by other services in the stack (PRIVATE)",
+			},
+			Default: serviceType,
+		}
+
+		err = survey.AskOne(prompt2, &serviceType)
+		if err != nil {
+			return nil, errors.Wrapf(err, "failed to obtain an aws profile")
+		}
+
+		serviceType = regexp.MustCompile(`\((.*?)\)`).FindStringSubmatch(serviceType)[1]
+
+		port := ""
+		prompt3 := &survey.Input{
+			Message: fmt.Sprintf("Which port does service %s listen on?", serviceName),
+			Default: "3000",
+		}
+		err = survey.AskOne(prompt3, &port, survey.WithValidator(survey.Required), survey.WithValidator(survey.MinLength(2)), survey.WithValidator(Port))
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to prompt")
+		}
+
+		uri := ""
+		prompt3 = &survey.Input{
+			Message: fmt.Sprintf("Which uri does %s respond on?", serviceName),
+			Help:    "This is the relative path that the service will respond on, e.g. /api/v1",
+			Default: "/",
+		}
+		err = survey.AskOne(prompt3, &uri, survey.WithValidator(survey.Required), survey.WithValidator(survey.MinLength(1)), survey.WithValidator(URI))
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to prompt")
+		}
+
+		uri, _ = strings.CutSuffix(uri, "/")
+		portNumber, err := strconv.Atoi(port)
+
+		if err != nil {
+			return nil, errors.Wrap(err, "port number is not valid")
+		}
+
+		services = append(services, Service{
+			Name:           serviceName,
+			ServiceType:    serviceType,
+			DockerfilePath: dockerFileName,
+			Context:        contextPath,
+			Port:           portNumber,
+			Uri:            uri,
+		})
+	}
+
 	happyConfig.GetData().Environments = environments
 	happyConfig.GetData().FeatureFlags = config.Features{
 		EnableDynamoLocking:   true,
@@ -220,29 +293,32 @@ func CreeateHappyConfig(ctx context.Context, bootstrapConfig *config.Bootstrap) 
 		"stack_defaults":   "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=main",
 		"routing_method":   "CONTEXT",
 		"create_dashboard": false,
+		"app":              appName,
 		"services":         serviceDefs,
 	}
 
 	serviceNames := []string{}
+	// sort services by length of service.Uri in reverse order
+	sort.Slice(services, func(i, j int) bool {
+		return len(services[i].Uri) > len(services[j].Uri)
+	})
+	priority := 0
+	for _, service := range services {
+		service.Priority = priority
+		priority++
+	}
+
 	for _, service := range services {
 		serviceDefs[service.Name] =
 			map[string]any{
-				"name":                             "frontend",
-				"desired_count":                    1,
-				"max_count":                        1,
-				"scaling_cpu_threshold_percentage": 80,
-				"port":                             3000,
-				"memory":                           "128Mi",
-				"cpu":                              "100m",
-				"health_check_path":                "/",
-				"service_type":                     service.ServiceType,
-				"path":                             "/*",
-				"priority":                         0,
-				"success_codes":                    "200-499",
-				"initial_delay_seconds":            30,
-				"period_seconds":                   3,
-				"platform_architecture":            "arm64",
-				"synthetics":                       false,
+				"name":                  service.Name,
+				"port":                  service.Port,
+				"health_check_path":     service.Uri,
+				"service_type":          service.ServiceType,
+				"path":                  fmt.Sprintf("%s/*", service.Uri),
+				"priority":              service.Priority,
+				"success_codes":         "200-499",
+				"platform_architecture": "arm64",
 				"build": map[string]any{
 					"context":    service.Context,
 					"dockerfile": service.DockerfilePath,
@@ -325,4 +401,26 @@ func findAllDockerfiles(path string) ([]string, error) {
 	})
 
 	return paths, err
+}
+
+func Port(val interface{}) error {
+	if str, ok := val.(string); ok {
+		if !govalidator.IsPort(str) {
+			return fmt.Errorf("value is not a valid port number")
+		}
+	} else {
+		return fmt.Errorf("cannot enforce a port number check on response of type %v", reflect.TypeOf(val).Name())
+	}
+	return nil
+}
+
+func URI(val interface{}) error {
+	if str, ok := val.(string); ok {
+		if !govalidator.IsRequestURI(str) {
+			return fmt.Errorf("value is not a valid uri")
+		}
+	} else {
+		return fmt.Errorf("cannot enforce a uri check on response of type %v", reflect.TypeOf(val).Name())
+	}
+	return nil
 }
