5891f42a8a2953f8698df0694a5a3c94992ad0e2
Jake Heath <jheath@chanzuckerberg.com>
2023-06-20 12:40:39 -0400

    save

--- api/pkg/api/app.go
+++ api/pkg/api/app.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"time"
 
@@ -29,6 +30,21 @@ func MakeAPIApplication(cfg *setup.Configuration) *APIApplication {
 			AppName:        "happy-api",
 			ReadTimeout:    60 * time.Second,
 			ReadBufferSize: 1024 * 64,
+			ErrorHandler: func(c *fiber.Ctx, err error) error {
+				// Status code defaults to 500
+				code := fiber.StatusInternalServerError
+
+				// Retrieve the custom status code if it's a *fiber.Error
+				var e *fiber.Error
+				if errors.As(err, &e) {
+					code = e.Code
+				}
+
+				// Set Content-Type: text/plain; charset=utf-8
+				c.Set(fiber.HeaderContentType, fiber.MIMETextPlainCharsetUTF8)
+				// Return status code with error message
+				return c.Status(code).SendString(err.Error())
+			},
 		}),
 		Cfg: cfg,
 	}
