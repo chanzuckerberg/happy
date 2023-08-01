7d370c8018af7f6744ddac5fa7a492e9c2fb9515
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2023-08-01 12:40:43 -0700

    feat: expose env vars and cron vars to stack (#2098)
    * feat: expose env vars and cron vars to stack
    
    * optional cpu and memory

--- terraform/modules/happy-stack-eks/variables.tf
+++ terraform/modules/happy-stack-eks/variables.tf
@@ -161,10 +161,12 @@ variable "services" {
 variable "tasks" {
   type = map(object({
     image : string,
-    memory : string,
-    cpu : string,
-    cmd : set(string),
+    memory : optional(string, "10Mi"),
+    cpu : optional(string, "10m"),
+    cmd : optional(set(string), []),
     platform_architecture : optional(string, "amd64"), // Supported values: amd64, arm64
+    is_cron_job : optional(bool, false),
+    cron_schedule : optional(string, "0 0 1 1 *"),
   }))
   description = "The deletion/migration tasks you want to run when a stack comes up and down."
   default     = {}
