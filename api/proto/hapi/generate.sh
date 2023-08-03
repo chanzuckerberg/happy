065fc09df00950f051acb8f225bb168c2987a8b4
David Spadea <dspadea@chanzuckerberg.com>
2023-08-03 13:37:50 -0400

    Add Typescript generation

--- api/proto/hapi/generate.sh
+++ api/proto/hapi/generate.sh
@@ -3,12 +3,14 @@
 SRC_DIR=.
 GO_DST_DIR=go/
 
-rm -rf go
+rm -rf go grpcweb
 mkdir go
+mkdir grpcweb
 
 for proto in *.proto
 do
     set -x
     protoc -I=$SRC_DIR --go_out=$GO_DST_DIR $SRC_DIR/$proto
+    protoc -I . --grpc-web_out=import_style=typescript,mode=grpcwebtext:grpcweb $proto
     set +x
 done
