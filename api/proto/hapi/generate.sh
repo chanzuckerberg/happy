1d3959581f250236599a5567a2c8ffd5dee40c97
David Spadea <dspadea@chanzuckerberg.com>
2023-08-03 13:51:32 -0400

    Add JS generation

--- api/proto/hapi/generate.sh
+++ api/proto/hapi/generate.sh
@@ -2,15 +2,17 @@
 
 SRC_DIR=.
 GO_DST_DIR=go/
+JS_DST_DIR=js/
+TS_DST_DIR=ts/
 
-rm -rf go grpcweb
-mkdir go
-mkdir grpcweb
+rm -rf $GO_DST_DIR $JS_DST_DIR $TS_DST_DIR
+mkdir  $GO_DST_DIR $JS_DST_DIR $TS_DST_DIR
 
 for proto in *.proto
 do
     set -x
     protoc -I=$SRC_DIR --go_out=$GO_DST_DIR $SRC_DIR/$proto
-    protoc -I . --grpc-web_out=import_style=typescript,mode=grpcwebtext:grpcweb $proto
+    protoc -I=$SRC_DIR --grpc-web_out=import_style=typescript,mode=grpcwebtext:$TS_DST_DIR $proto
+    protoc -I=$SRC_DIR --js_out=import_style=commonjs:$JS_DST_DIR  --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$JS_DST_DIR $proto
     set +x
 done
