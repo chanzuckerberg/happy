6382499c3de4ef354bb79c746a59d89284dc2b79
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-08-29 08:55:15 -0400

    Updated aws provider version

--- terraform/modules/happy-route53/versions.tf
+++ terraform/modules/happy-route53/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.45"
+      version = ">= 5.14"
 
       configuration_aliases = [aws.czi-si]
     }
