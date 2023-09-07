c9d861670f9acc05dd5d0883902231ddb05c20c1
Annie Ku <anne.ku@chanzuckerberg.com>
2023-09-07 10:36:02 -0700

    fix: de-duplicate waf attachment

--- terraform/modules/happy-env-ecs/lb-public.tf
+++ terraform/modules/happy-env-ecs/lb-public.tf
@@ -106,8 +106,8 @@ resource "aws_lb_listener" "public-http" {
   }
 }
 
-resource "aws_wafv2_web_acl_association" "private" {
-  for_each     = local.private_services
-  resource_arn = aws_lb.lb-private[each.key].arn
+resource "aws_wafv2_web_acl_association" "public" {
+  for_each     = local.public_services
+  resource_arn = aws_lb.lb-public[each.key].arn
   web_acl_arn  = var.regional_wafv2_arn
 }
