2fff7ee61865a3dd0a21622dfbb8a5e9aa439b12
jakeyheath <jakeyheath@users.noreply.github.com>
2023-09-15 21:22:36 +0000

    commit from ci -- ran terraform fmt and pushed

--- terraform/modules/happy-env-eks/oidc.tf
+++ terraform/modules/happy-env-eks/oidc.tf
@@ -5,11 +5,11 @@ module "happy_okta_app" {
   env      = var.tags.env
   # backward compatibility
   # todo: remove var.okta_teams for var.oidc_config.teams
-  teams         = coalesce(var.okta_teams, var.oidc_config.teams)
-  base_domain   = data.aws_route53_zone.base_zone.name
-  redirect_uris = var.oidc_config.redirect_uris
-  login_uri     = var.oidc_config.login_uri
-  grant_types   = var.oidc_config.grant_types
-  app_type      = var.oidc_config.app_type
+  teams                      = coalesce(var.okta_teams, var.oidc_config.teams)
+  base_domain                = data.aws_route53_zone.base_zone.name
+  redirect_uris              = var.oidc_config.redirect_uris
+  login_uri                  = var.oidc_config.login_uri
+  grant_types                = var.oidc_config.grant_types
+  app_type                   = var.oidc_config.app_type
   token_endpoint_auth_method = var.oidc_config.token_endpoint_auth_method
 }
