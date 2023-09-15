1e5b66e528ddc29643e0e00beea7be6d2ae77ce9
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2023-09-15 13:43:12 -0700

    fix: allow for changing the default token endpoint auth (#2458)
    * fix: allow for changing the default token endpoint auth
    
    * commit from ci -- ran terraform-docs and pushed
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/modules/happy-env-eks/variables.tf
+++ terraform/modules/happy-env-eks/variables.tf
@@ -125,11 +125,12 @@ variable "waf_arn" {
 
 variable "oidc_config" {
   type = object({
-    login_uri     = optional(string, ""),
-    grant_types   = optional(set(string), ["authorization_code", "refresh_token"])
-    redirect_uris = optional(set(string), []),
-    teams         = optional(set(string), []),
-    app_type      = optional(string, "web"),
+    login_uri                  = optional(string, ""),
+    grant_types                = optional(set(string), ["authorization_code", "refresh_token"])
+    redirect_uris              = optional(set(string), []),
+    teams                      = optional(set(string), []),
+    app_type                   = optional(string, "web"),
+    token_endpoint_auth_method = optional(string, "client_secret_basic"),
   })
   default     = {}
   description = "OIDC configuration for the happy stacks in this environment."
