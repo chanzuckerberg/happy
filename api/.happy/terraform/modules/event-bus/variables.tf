b0911aa101879b2a24b45ba18e26b2520f86b25f
Hayden Spitzley <hspitzley@chanzuckerberg.com>
2023-09-21 12:48:48 -0400

    feat: create sns topic and subscribed sqs queue for happy events

---
+++ api/.happy/terraform/modules/event-bus/variables.tf
@@ -0,0 +1,8 @@
+variable "stack_name" {
+  type        = string
+  description = "Happy Path stack name"
+}
+variable "k8s_namespace" {
+  type        = string
+  description = "K8S namespace for this stack"
+}
