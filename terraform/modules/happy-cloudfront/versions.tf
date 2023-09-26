e404d14e2ace798d5a1c3fc023c8354a4692cfe1
Jake Heath <jheath@chanzuckerberg.com>
2023-09-26 15:41:43 -0700

    random provider

--- terraform/modules/happy-cloudfront/versions.tf
+++ terraform/modules/happy-cloudfront/versions.tf
@@ -6,6 +6,10 @@ terraform {
 
       configuration_aliases = [aws.useast1]
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
   }
   required_version = ">= 1.3"
 }
