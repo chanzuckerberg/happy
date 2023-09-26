6b6e8e16f8ae5fc805676b093dff783ddabb9e28
Jake Heath <jheath@chanzuckerberg.com>
2023-09-25 21:01:52 -0700

    remove service tags

--- terraform/modules/happy-service-eks/tags.tf
+++ terraform/modules/happy-service-eks/tags.tf
@@ -1,4 +1,5 @@
 locals {
+  happy_last_applied = timestamp()
   tags = merge(var.tags, {
     happy_env          = var.deployment_stage,
     happy_stack_name   = var.stack_name,
@@ -6,15 +7,16 @@ locals {
     happy_region       = data.aws_region.current.name,
     happy_image_tag    = var.image_tag
     happy_service_type = var.routing.service_type,
-    happy_last_applied = timestamp(),
+    happy_last_applied = local.happy_last_applied,
   })
 
+  # the tags have to be exactly the same across all ingresses in the ingress group
+  # in context based routing, different service will share the same ingress group
+  # so don't include their service specific tag information or the ALB won't be created
   routing_tags = merge(var.tags, {
     happy_env          = var.deployment_stage,
     happy_stack_name   = var.stack_name,
     happy_region       = data.aws_region.current.name,
-    happy_last_applied = timestamp(),
-    happy_service_type = var.routing.service_type,
-    happy_service_name = var.routing.service_name,
+    happy_last_applied = local.happy_last_applied,
   })
 }
