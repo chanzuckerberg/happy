ab59068192a1fba602bdf35316d266a17e8c9d3a
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-10-23 10:54:17 -0400

    Update main.tf

--- examples/integration_test/.happy/terraform/envs/rdev/main.tf
+++ examples/integration_test/.happy/terraform/envs/rdev/main.tf
@@ -1,5 +1,5 @@
 module "stack" {
-  source = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=alokshin/CCIE-1977-support-pod-disruption-budgets"
+  source = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=main"
 
   image_tag        = var.image_tag
   image_tags       = jsondecode(var.image_tags)
