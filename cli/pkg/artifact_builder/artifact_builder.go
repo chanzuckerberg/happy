e2c7363d2912732d85e18fa3818dac703b71f91f
Jake Heath <76011913+jakeyheath@users.noreply.github.com>
2023-10-26 10:53:10 -0700

    Update artifact_builder.go

--- cli/pkg/artifact_builder/artifact_builder.go
+++ cli/pkg/artifact_builder/artifact_builder.go
@@ -566,7 +566,7 @@ func (ab ArtifactBuilder) Push(ctx context.Context, tags []string) error {
 // PushFrom allows the caller to specify where the images are coming from and also what tags
 // to pull from.
 func (ab ArtifactBuilder) PushFromWithTag(ctx context.Context, servicesImage map[string]string, tag string) error {
-	err = ab.RegistryLogin(ctx)
+	err := ab.RegistryLogin(ctx)
 	if err != nil {
 		return err
 	}
