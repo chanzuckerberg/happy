f56e114f54241aa7dfe05238b89013471de638a8
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-10-26 14:39:32 -0400

    fix: Update the pin to avoid caching

--- examples/integration_test/.happy/terraform/envs/rdev/main.tf
+++ examples/integration_test/.happy/terraform/envs/rdev/main.tf
@@ -1,5 +1,5 @@
 module "stack" {
-  source = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=main"
+  source = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=v0.114.0"
 
   image_tag        = var.image_tag
   image_tags       = jsondecode(var.image_tags)
