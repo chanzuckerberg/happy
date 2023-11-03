0a8fd58331521ba7952dad41660d81836a6c5bf6
David Spadea <dspadea@chanzuckerberg.com>
2023-11-03 16:11:07 -0400

    Add stub version command

---
+++ hvm/cmd/version.go
@@ -0,0 +1,27 @@
+/*
+HVM Version Commands
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+// installCmd represents the install command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Output version of HVM",
+	Long:  `Output the current version of the HVM CLI`,
+	RunE:  outputVersion,
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+func outputVersion(cmd *cobra.Command, args []string) error {
+
+	fmt.Println("PRINTING VERSION HERE")
+	return nil
+}
