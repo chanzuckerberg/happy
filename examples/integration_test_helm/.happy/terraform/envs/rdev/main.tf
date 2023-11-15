d442d182902a07bf1e4413b1775b40ab65a445d4
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-11-15 14:29:55 -0500

    Per code review

--- examples/integration_test_helm/.happy/terraform/envs/rdev/main.tf
+++ examples/integration_test_helm/.happy/terraform/envs/rdev/main.tf
@@ -1,5 +1,5 @@
 module "stack" {
-  source = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-helm-eks?ref=alokshin/CCIE-1420"
+  source = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-helm-eks?ref=main"
 
   image_tag        = var.image_tag
   image_tags       = jsondecode(var.image_tags)
