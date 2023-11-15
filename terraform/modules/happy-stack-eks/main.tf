8e07af76cfe422a2ea36a88f6d08bdc70aa51e54
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-11-15 12:26:33 -0500

    Bump

--- terraform/modules/happy-stack-eks/main.tf
+++ terraform/modules/happy-stack-eks/main.tf
@@ -72,7 +72,7 @@ locals {
       replace(v.image, "/{(${join("|", keys(local.service_ecrs))})}/", "%s"),
       [
         for repo in flatten(regexall("{(${join("|", keys(local.service_ecrs))})}", v.image)) :
-        lookup(local.service_ecrs, repo)
+        lookup(local.service_ecrs, repo, "")
       ]...
     )
   }) }
