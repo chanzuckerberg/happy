88d142a06cc3a447043bde65a411d3c25810dd3c
Hayden Spitzley <105455169+hspitzley-czi@users.noreply.github.com>
2024-01-08 08:25:52 -0700

    feat: add command to migrate all configs from v1 to v2 (#2903)

--- cli/cmd/config_migrate.go
+++ cli/cmd/config_migrate.go
@@ -8,6 +8,7 @@ import (
 	"github.com/chanzuckerberg/happy/cli/pkg/hapi"
 	"github.com/chanzuckerberg/happy/shared/client"
 	apiclient "github.com/chanzuckerberg/happy/shared/hapi"
+	"github.com/chanzuckerberg/happy/shared/model"
 	"github.com/chanzuckerberg/happy/shared/util"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -16,6 +17,7 @@ import (
 
 func init() {
 	configCmd.AddCommand(configMigrateCmd)
+	configCmd.AddCommand(configMigrateAllCmd)
 }
 
 var configMigrateCmd = &cobra.Command{
@@ -60,38 +62,80 @@ var configMigrateCmd = &cobra.Command{
 			return err
 		}
 
-		// Write the value to the v2 api
-		awsCredsProvider := hapi.NewAWSCredentialsProviderCLI(happyClient.AWSBackend)
-		creds, err := awsCredsProvider.GetCredentials(context.Background())
-		if err != nil {
-			return errors.Wrap(err, "failed to get aws credentials")
-		}
+		return migrateConfig(happyClient, result.Record)
+	},
+}
 
-		params := &apiclient.SetAppConfigParams{
-			AppName:             happyClient.HappyConfig.App(),
-			Environment:         happyClient.HappyConfig.GetEnv(),
-			Stack:               &stack,
-			AwsProfile:          *happyClient.HappyConfig.AwsProfile(),
-			AwsRegion:           *happyClient.HappyConfig.AwsRegion(),
-			K8sNamespace:        happyClient.HappyConfig.K8SConfig().Namespace,
-			K8sClusterId:        happyClient.HappyConfig.K8SConfig().ClusterID,
-			XAwsAccessKeyId:     b64.StdEncoding.EncodeToString([]byte(creds.AccessKeyID)),
-			XAwsSecretAccessKey: b64.StdEncoding.EncodeToString([]byte(creds.SecretAccessKey)),
-			XAwsSessionToken:    creds.SessionToken, // SessionToken is already base64 encoded
-		}
-		apiv2 := hapi.MakeAPIClientV2(happyClient.HappyConfig)
-		resp, err := apiv2.SetAppConfigWithResponse(context.Background(), params, apiclient.SetAppConfigJSONRequestBody{Key: key, Value: result.Record.Value})
+var configMigrateAllCmd = &cobra.Command{
+	Use:          "migrate-all",
+	Short:        "migrate all config values from v1 to v2",
+	Long:         "Migrate all configs for the given app, env, and stack from v1 to v2",
+	SilenceUsage: true,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		checklist := util.NewValidationCheckList()
+		return util.ValidateEnvironment(cmd.Context(),
+			checklist.TerraformInstalled,
+			checklist.AwsInstalled,
+		)
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		happyClient, err := makeHappyClient(cmd, sliceName, "", []string{}, false)
 		if err != nil {
 			return err
 		}
-		if resp.HTTPResponse.StatusCode >= 400 {
-			return errors.New(string(resp.Body))
+
+		// Get the values from the v1 api
+		api := hapi.MakeAPIClient(happyClient.HappyConfig, happyClient.AWSBackend)
+		results, err := api.ListConfigs(happyClient.HappyConfig.App(), happyClient.HappyConfig.GetEnv(), stack)
+		if err != nil {
+			return err
 		}
 
-		logrus.Info(messageWithStackSuffix(
-			fmt.Sprintf("successfully migrated app config with key '%s' in environment '%s'", resp.JSON200.Key, happyClient.HappyConfig.GetEnv()),
-		))
+		for _, record := range results.Records {
+			logrus.Info(messageWithStackSuffix(
+				fmt.Sprintf("migrating app config with key '%s' in environment '%s'", record.Key, happyClient.HappyConfig.GetEnv()),
+			))
 
+			err := migrateConfig(happyClient, record)
+			if err != nil {
+				return err
+			}
+		}
 		return nil
 	},
+}
+
+func migrateConfig(happyClient *HappyClient, record *model.ResolvedAppConfig) error {
+	// Write the value to the v2 api
+	awsCredsProvider := hapi.NewAWSCredentialsProviderCLI(happyClient.AWSBackend)
+	creds, err := awsCredsProvider.GetCredentials(context.Background())
+	if err != nil {
+		return errors.Wrap(err, "failed to get aws credentials")
+	}
+
+	params := &apiclient.SetAppConfigParams{
+		AppName:             happyClient.HappyConfig.App(),
+		Environment:         happyClient.HappyConfig.GetEnv(),
+		Stack:               &record.Stack,
+		AwsProfile:          *happyClient.HappyConfig.AwsProfile(),
+		AwsRegion:           *happyClient.HappyConfig.AwsRegion(),
+		K8sNamespace:        happyClient.HappyConfig.K8SConfig().Namespace,
+		K8sClusterId:        happyClient.HappyConfig.K8SConfig().ClusterID,
+		XAwsAccessKeyId:     b64.StdEncoding.EncodeToString([]byte(creds.AccessKeyID)),
+		XAwsSecretAccessKey: b64.StdEncoding.EncodeToString([]byte(creds.SecretAccessKey)),
+		XAwsSessionToken:    creds.SessionToken, // SessionToken is already base64 encoded
+	}
+	apiv2 := hapi.MakeAPIClientV2(happyClient.HappyConfig)
+	resp, err := apiv2.SetAppConfigWithResponse(context.Background(), params, apiclient.SetAppConfigJSONRequestBody{Key: record.Key, Value: record.Value})
+	if err != nil {
+		return err
+	}
+	if resp.HTTPResponse.StatusCode >= 400 {
+		return errors.New(string(resp.Body))
+	}
+
+	logrus.Info(messageWithStackSuffix(
+		fmt.Sprintf("successfully migrated app config with key '%s' in environment '%s'", resp.JSON200.Key, happyClient.HappyConfig.GetEnv()),
+	))
+	return nil
 }
