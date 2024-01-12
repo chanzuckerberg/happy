40fed51222f9f5ff86313c802fca1eebbddefb09
Jake Heath <jheath@chanzuckerberg.com>
2024-01-12 09:57:11 -0800

    fix: make synthetics use additional_hostnames

--- terraform/modules/happy-stack-eks/synthetics.tf
+++ terraform/modules/happy-stack-eks/synthetics.tf
@@ -2,10 +2,15 @@ locals {
   # this is making an assumption that the health check path is accessible to the internet
   # if this is a non-prod OIDC protected stack, make sure to allow the health check endpoint
   # through the OIDC proxy.
-  synthetics = { for k, v in local.service_definitions : v.service_name =>
+  base_synthetics = { for k, v in local.service_definitions : v.service_name =>
     var.routing_method == "DOMAIN" ? "https://${v.service_name}.${local.external_dns}${v.health_check_path}" : "https://${var.stack_name}.${local.external_dns}${v.health_check_path}"
     if v.synthetics && (v.service_type == "EXTERNAL" || v.service_type == "INTERNAL")
   }
+
+  # If you are using a custom domain, you can add additional_hostnames. This will make only 1 synthetic per additonal_hostname
+  # and no synthetics to the internal URLs created for the services.
+  synthetics = length(var.additional_hostnames) == 0 ? local.base_synthetics : { for v in var.additional_hostnames : v => v }
+
   opsgenie_owner = "${local.secret["tags"]["project"]}-${local.secret["tags"]["env"]}-${local.secret["tags"]["service"]}"
 }
 
