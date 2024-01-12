d9f409b6b470ad0a88deb3bf10cb38ec3e89bf05
Jake Heath <jheath@chanzuckerberg.com>
2024-01-12 10:50:40 -0800

    synthetics

--- terraform/modules/happy-stack-eks/synthetics.tf
+++ terraform/modules/happy-stack-eks/synthetics.tf
@@ -7,14 +7,13 @@ locals {
     if v.synthetics && (v.service_type == "EXTERNAL" || v.service_type == "INTERNAL")
   }
 
-  additional_hosts_synthetics = { for k, v in local.service_definitions : 
-    { for domain in var.additional_hostnames :  v.service_name => "https://${domain}${v.health_check_path}" : "https://${var.stack_name}.${local.external_dns}${v.health_check_path}"
-    if v.synthetics && (v.service_type == "EXTERNAL" || v.service_type == "INTERNAL")
-  }
+  additional_hosts_synthetics = merge([for k, v in local.service_definitions :
+    { for domain in var.additional_hostnames : v.service_name => "https://${domain}${v.health_check_path}" }
+  ]...)
 
   # If you are using a custom domain, you can add additional_hostnames. This will make only 1 synthetic per additonal_hostname
   # and no synthetics to the internal URLs created for the services.
-  synthetics = length(var.additional_hostnames) == 0 ? local.base_synthetics : { for v in var.additional_hostnames : v => "https://${v}${v.health_check_path}" }
+  synthetics = length(var.additional_hostnames) == 0 ? local.base_synthetics : local.additional_hosts_synthetics
 
   opsgenie_owner = "${local.secret["tags"]["project"]}-${local.secret["tags"]["env"]}-${local.secret["tags"]["service"]}"
 }
