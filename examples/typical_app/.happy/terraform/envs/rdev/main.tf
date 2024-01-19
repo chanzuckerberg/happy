094cb1b9a37ac41e5ee22294a21ef1fcdb9ff0e1
Jake Heath <jheath@chanzuckerberg.com>
2024-01-19 11:31:20 -0800

    main branch

--- examples/typical_app/.happy/terraform/envs/rdev/main.tf
+++ examples/typical_app/.happy/terraform/envs/rdev/main.tf
@@ -1,5 +1,5 @@
 module "stack" {
-  source = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=heathj/additional-hosts-synthetics"
+  source = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=main"
 
   image_tag           = var.image_tag
   image_tags          = jsondecode(var.image_tags)
