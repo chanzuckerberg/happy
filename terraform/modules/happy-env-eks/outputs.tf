3f85359e9bb467572da0488fef3c8e4544ba04c3
Alex Lokshin <alokshin@chanzuckerberg.com>
2025-12-16 13:45:13 -0500

    Update outputs.tf

--- terraform/modules/happy-env-eks/outputs.tf
+++ terraform/modules/happy-env-eks/outputs.tf
@@ -2,14 +2,6 @@ output "namespace" {
   value = local.k8s_namespace
 }
 
-
-output "dashboard" {
-  value = {
-    id  = ""
-    url = ""
-  }
-}
-
 output "integration_secret" {
   value     = local.secret_string
   sensitive = true
